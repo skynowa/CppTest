7fce20b2d913add4d59f6288e0bd1e306d098aa3
skynowa <skynowa@gmail.com>
2012-09-17 16:27:00 +0300

    ADD: CstrNull.cpp

---
+++ CstrNull/CstrNull.cpp
@@ -0,0 +1,35 @@
+/**
+ * \file  CstrNull.cpp
+ * \brief c_str() of empty std::string
+ */
+
+
+//---------------------------------------------------------------------------
+#include <unistd.h>
+#include <assert.h>
+#include <string>
+#include <iostream>
+//---------------------------------------------------------------------------
+int main(int iArgCount, char **paszArgs)
+{
+    std::string s;
+
+    const char *p = NULL;
+    assert(NULL == p);
+
+    p = s.c_str();
+    assert(NULL != p);
+
+    // std::cout << "" << std::endl;
+
+    return 0;
+}
+//---------------------------------------------------------------------------
+
+#if OUTPUT
+// NOTE:
+
+$ ./CstrNull
+    <no asserts>
+
+#endif
