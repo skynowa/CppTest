3a7f6bf3a3b7107942e0ef73d2d9d9a147e60655
skynowa <skynowa@gmail.com>
2012-09-17 16:41:34 +0300

    ADD: GlobalLocalVars.cpp

---
+++ GlobalLocalVars/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file  main.cpp
+ * \brief global and local var with the same name
+ */
+
+
+//---------------------------------------------------------------------------
+#include <unistd.h>
+#include <assert.h>
+#include <string>
+#include <iostream>
+//---------------------------------------------------------------------------
+std::string s = "string_global";
+//---------------------------------------------------------------------------
+int main(int iArgCount, char **paszArgs)
+{
+    std::string s = "string_local";
+
+    assert("string_global" == ::s);
+    std::cout << "Global s = " << ::s << std::endl;
+
+    assert("string_local" == s);
+    std::cout << "Local  s = " << s   << std::endl;
+
+    return 0;
+}
+//---------------------------------------------------------------------------
+
+#if OUTPUT
+// NOTE:
+
+> ./main
+
+    Global s = string_global
+    Local  s = string_local
+
+#endif
