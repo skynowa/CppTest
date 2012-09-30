626814e4af890283e62f917047786d142a6948b2
skynowa <skynowa@gmail.com>
2012-09-30 17:14:38 +0300

    ADD: StdOstringstreamOperatorIn

---
+++ StdOstringstreamOperatorIn/main.cpp
@@ -0,0 +1,52 @@
+/**
+ * \file  main.cpp
+ * \brief 
+ */
+
+
+//---------------------------------------------------------------------------
+#include <assert.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+//---------------------------------------------------------------------------
+namespace std {
+   typedef basic_string<unsigned char, char_traits<unsigned char>, allocator<unsigned char> > ustring_t;
+}
+//---------------------------------------------------------------------------
+std::basic_ostringstream<char>& 
+operator << (
+   std::basic_ostringstream<char> &osOut, 
+   const std::ustring_t           &cusValue
+)
+{
+    std::string sRv;
+
+    sRv.assign( cusValue.begin(), cusValue.end() );
+
+    osOut << sRv << std::flush;  
+
+    return osOut;
+}
+//---------------------------------------------------------------------------
+int main() 
+{
+    std::ostringstream ossStream;
+    std::ustring_t     sRv1(4, 'a'); 
+    std::ustring_t     sRv2(4, 'b'); 
+    
+    ossStream << sRv1 << sRv2;
+
+    std::cout << ossStream.str() << std::endl;
+         
+    return 0;
+}
+//---------------------------------------------------------------------------
+#if OUTPUT
+// NOTE:
+
+> ./main
+
+    aaaabbbb
+
+#endif
