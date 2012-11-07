443adcfe27b3e408bedcb0b22552433eb198395d
skynowa <skynowa@gmail.com>
2012-11-07 20:33:25 +0200

    ADD: SizeofClass ADD: templates

---
+++ SizeofClass/Main.cpp
@@ -0,0 +1,57 @@
+ /*
+ * \file  Main.cpp
+ * \brief 
+ */
+
+
+//---------------------------------------------------------------------------
+#include <string>
+#include <iostream>
+#include <assert.h>
+//---------------------------------------------------------------------------
+class CEmpty {
+
+};
+
+class CData {
+    void foo() { ; }
+};
+
+class CInterface {
+    virtual void foo() = 0;
+};
+
+class CVirtual {
+    virtual void foo() { ; }
+};
+
+class CDerivedVirtual {
+    virtual void foo() { ; }
+};
+//---------------------------------------------------------------------------
+int main(int iArgCount, char **paszArgs)
+{
+    std::cout << "sizeof(void *):          " << sizeof(void *)          << std::endl;
+    std::cout << "sizeof(CEmpty):          " << sizeof(CEmpty)          << std::endl;
+    std::cout << "sizeof(CData):           " << sizeof(CData)           << std::endl;
+    std::cout << "sizeof(CInterface):      " << sizeof(CInterface)      << std::endl;
+    std::cout << "sizeof(CVirtual:         " << sizeof(CVirtual)        << std::endl;
+    std::cout << "sizeof(CDerivedVirtual): " << sizeof(CDerivedVirtual) << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//---------------------------------------------------------------------------
+
+
+#if OUTPUT
+// NOTE:
+
+sizeof(void *):          4
+sizeof(CEmpty):          1
+sizeof(CData):           1
+sizeof(CInterface):      4
+sizeof(CVirtual:         4
+sizeof(CDerivedVirtual): 4
+
+
+#endif
---
+++ _Template/Main.cpp
@@ -0,0 +1,29 @@
+ /*
+ * \file  Main.cpp
+ * \brief 
+ */
+
+
+//---------------------------------------------------------------------------
+#include <string>
+#include <iostream>
+#include <assert.h>
+//---------------------------------------------------------------------------
+int main(int iArgCount, char **paszArgs)
+{
+
+
+    // std::cout << "" << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//---------------------------------------------------------------------------
+
+
+#if OUTPUT
+// NOTE:
+
+$ ./main
+
+
+#endif
