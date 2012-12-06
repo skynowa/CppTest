8c57fdfc9a32690082fe2fb6c2f4fb8708e9ade3
skynowa <skynowa@gmail.com>
2012-12-06 13:37:01 +0200

    REVIEW: EnumCodeStyle

--- EnumCodeStyle/Main.cpp
+++ EnumCodeStyle/Main.cpp
@@ -9,24 +9,51 @@
 #include <iostream>
 #include <assert.h>
 //---------------------------------------------------------------------------
+namespace NOpenMode {
+    enum EType {
+        Read1,
+        Read2,
+        READ3
+    };
+};
+//---------------------------------------------------------------------------
 class AClass {
     public:
         enum EOpenMode {
             omRead,
             OpenMode_Read,
-            OPEN_MODE_READ,
+            OPEN_MODE_READ
         };
 
         AClass() {}
 };
 //---------------------------------------------------------------------------
+void 
+foo1(NOpenMode::EType mode) {
+    std::cout << "mode: " << mode << std::endl;
+}
+//---------------------------------------------------------------------------
+void 
+foo2(AClass::EOpenMode mode) {
+    std::cout << "mode: " << mode << std::endl;
+}
+//---------------------------------------------------------------------------
 int main(int iArgCount, char **paszArgs)
 {
-    AClass a;
+    {
+        std::cout << NOpenMode::Read1 << std::endl;
+        std::cout << NOpenMode::Read2 << std::endl;
+        std::cout << NOpenMode::READ3 << std::endl;
+    }
 
-    std::cout << a.omRead         << std::endl;
-    std::cout << a.OpenMode_Read  << std::endl;
-    std::cout << a.OPEN_MODE_READ << std::endl;
+    {
+        AClass::EOpenMode mode = AClass::OPEN_MODE_READ;
+
+        std::cout << mode                   << std::endl;
+        std::cout << AClass::omRead         << std::endl;
+        std::cout << AClass::OpenMode_Read  << std::endl;
+        std::cout << AClass::OPEN_MODE_READ << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
