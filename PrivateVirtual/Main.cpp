e23b1976a57172520066131ae73d2f6258664a1d
skynowa <skynowa@gmail.com>
2013-01-09 14:46:30 +0200

    ADD: PrivateVirtual

---
+++ PrivateVirtual/Main.cpp
@@ -0,0 +1,51 @@
+ /*
+ * \file  Main.cpp
+ * \brief 
+ */
+
+
+//---------------------------------------------------------------------------
+#include <string>
+#include <iostream>
+#include <assert.h>
+//---------------------------------------------------------------------------
+class A {
+    public:
+        void bar() { 
+            foo(); 
+        }
+
+    private: /* or public */
+        virtual void foo() { 
+            std::cout << __FUNCTION__ << std::endl; 
+        }
+};
+//---------------------------------------------------------------------------
+class B: 
+    public A 
+{
+    private: /* or public */
+        virtual void foo() { 
+            std::cout << __FUNCTION__ << std::endl; 
+        }
+};
+//---------------------------------------------------------------------------
+int main(int iArgCount, char **paszArgs)
+{
+    A *a = new B();
+    a->bar();
+
+    delete a;
+
+    // std::cout << "" << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//---------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+ B::foo
+
+#endif
