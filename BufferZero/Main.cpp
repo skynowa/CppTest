c5eb8406f4dcb41f8af3eee66d1a3537e5940f74
skynowa <skynowa@gmail.com>
2013-01-13 22:27:05 +0200

    ADD: BufferZero

---
+++ BufferZero/Main.cpp
@@ -0,0 +1,78 @@
+﻿ /*
+ * \file  Main.cpp
+ * \brief 
+ */
+
+
+//---------------------------------------------------------------------------
+#include <string>
+#include <iostream>
+#include <assert.h>
+//---------------------------------------------------------------------------
+int main(int iArgCount, char **paszArgs)
+{
+    const size_t cuiBuffSize = 5;
+
+   {
+        char szBuff[cuiBuffSize] = {0};
+
+        for (size_t i = 0; i < cuiBuffSize; ++ i) {
+            std::cout << "["<< szBuff[0] << "]" << std::endl;
+        }
+
+        std::cout << std::endl;
+
+    #if OUTPUT
+
+        [ ]
+        [ ]
+        [ ]
+        [ ]
+        [ ]
+
+    #endif
+    }
+
+    {
+        char *pszBuff = new char[cuiBuffSize];
+
+        for (size_t i = 0; i < cuiBuffSize; ++ i) {
+            std::cout << "["<< pszBuff[0] << "]" << std::endl;
+        }
+
+        std::cout << std::endl;
+
+    #if OUTPUT
+
+        [═]
+        [═]
+        [═]
+        [═]
+        [═]
+
+    #endif
+    }
+
+    {
+        char *pszBuff = new char[cuiBuffSize]();
+
+        for (size_t i = 0; i < cuiBuffSize; ++ i) {
+            std::cout << "["<< pszBuff[0] << "]" << std::endl;
+        }
+
+        std::cout << std::endl;
+
+    #if OUTPUT
+
+        [ ]
+        [ ]
+        [ ]
+        [ ]
+        [ ]
+
+    #endif
+    }
+
+    return EXIT_SUCCESS;
+}
+//---------------------------------------------------------------------------
