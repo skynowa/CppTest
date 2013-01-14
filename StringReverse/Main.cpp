0df7be0134400ee834e27b8b2ab9c8a1c2f2625b
skynowa <skynowa@gmail.com>
2013-01-14 22:09:21 +0200

    ADD: StringReverse

---
+++ StringReverse/Main.cpp
@@ -0,0 +1,30 @@
+ /*
+ * \file  Main.cpp
+ * \brief 
+ */
+
+
+//---------------------------------------------------------------------------
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <algorithm>
+//---------------------------------------------------------------------------
+int main()
+{
+   char str[] = "abcde";
+   
+   std::reverse(&str[0], &str[5]);
+   
+   std::cout << str << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//---------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+    edcba
+
+#endif
