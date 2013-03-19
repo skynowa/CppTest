4f7d1c6d0b8894c6e43b5f4039c83f34ce065592
skynowa <skynowa@gmail.com>
2013-03-19 15:03:03 +0200

    ADD: CString

---
+++ CString/CString.h
@@ -0,0 +1,93 @@
+﻿#include <string.h>
+#include <iostream>
+
+class CString
+{
+public:
+                         CString               ();
+                         CString               (const char *str);
+                         CString               (const CString &str);
+                        ~CString               ();
+
+                         operator char *       ();
+                         operator const char * ();
+   CString               operator +            (const CString &q) const;
+   CString &             operator =            (const CString &q);
+   // don't forget comparison operators
+   friend std::istream & operator >>           (std::istream &is, CString &obj);
+   friend std::ostream & operator <<           (std::ostream &os, const CString &obj);
+
+private:
+    char *cstr;    
+};
+//------------------------------------------------------------------------------
+CString::CString() {
+    cstr = NULL;
+}
+//------------------------------------------------------------------------------
+CString::CString(const char *str) {
+    cstr = new char[strlen(str) + 1]();
+    strcpy(cstr,str);
+}
+//------------------------------------------------------------------------------
+CString::CString(const CString &q) {
+    if (*this == q) {
+        return;
+    }
+    
+    cstr = new char[strlen(q.cstr) + 1]();
+    strcpy(cstr,q.cstr);
+}
+//------------------------------------------------------------------------------
+CString::~CString() {
+    if (cstr) {
+        delete[] cstr;
+        cstr = NULL;
+    }
+}
+//------------------------------------------------------------------------------
+CString::operator char * () {
+    return cstr;
+}
+//------------------------------------------------------------------------------
+CString::operator const char * () {
+    return cstr;
+}
+//------------------------------------------------------------------------------
+CString 
+CString::operator + (const CString &q) const {
+    CString s;
+    s.cstr = new char[strlen(cstr) + strlen(q.cstr) + 1]();
+    strcpy(s.cstr, cstr);
+    strcat(s.cstr, q.cstr);
+    
+    return s;
+}
+//------------------------------------------------------------------------------
+CString & 
+CString::operator = (const CString &q) {
+    if (*this != q) {
+        if (cstr) {
+            delete[] cstr;
+            cstr = NULL;
+        }
+        
+        cstr = new char[strlen(q.cstr) + 1]();
+        strcpy(cstr, q.cstr);
+    }
+    
+    return *this;
+}
+//------------------------------------------------------------------------------
+std::istream & 
+operator >> (std::istream &is, CString &obj) {
+   is >> strVal;
+   return is;
+}
+//------------------------------------------------------------------------------
+std::ostream & 
+operator << (std::ostream &os, const CString &obj) {
+   os << obj;
+   return os;
+}
+//------------------------------------------------------------------------------
---
+++ CString/main.cpp
@@ -0,0 +1,30 @@
+ /*
+ * \file  main.cpp
+ * \brief 
+ */
+
+
+//---------------------------------------------------------------------------
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include "CString.h"
+//---------------------------------------------------------------------------
+int main(int iArgCount, char **paszArgs)
+{
+    CString s;
+   
+    std::cin  >> s;
+    std::cout << "Printing the value\n";
+    std::cout << s << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//---------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
