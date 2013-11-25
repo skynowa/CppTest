982bb1bcdf352a5d23afc94da99ce8f6cf5460c2
skynowa <skynowa@gmail.com>
2013-11-25 12:31:43 +0200

    SQL - update

---
+++ _SQL/test.sql
@@ -0,0 +1,11 @@
+SQL
+
+
+// increment
+UPDATE T_PERSON SET F_ADGE = F_ADGE + 10;
+
+// show last N records
+SELECT * FROM triptake.BookedBookingInfos ORDER BY id DESC LIMIT 3;
+
+// show duplicates
+SELECT COUNT(*), Code FROM triptake.AGdsHotels where gdsid=22 GROUP BY Code HAVING COUNT(*)>=2
