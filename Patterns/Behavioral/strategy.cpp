f7f95fd338872120ef8abb98cc5ecea9b6d012c8
skynowa <skynowa@gmail.com>
2015-08-14 15:53:45 +0300

    Patterns: strategy - update

--- Patterns/Behavioral/strategy.cpp
+++ Patterns/Behavioral/strategy.cpp
@@ -3,16 +3,18 @@ Strategy is an algorithm that you pass into a function or class that uses the st
 The idea is that you can provide a different strategy to achieve something different.
 */
 
+
 #include <iostream>
 #include <string>
 
+//-------------------------------------------------------------------------------------------------
 class IStrategy
 {
 public:
 	virtual ~IStrategy() { }
 	virtual std::string format(const std::string &, const std::string &) const = 0;
 };
-
+//-------------------------------------------------------------------------------------------------
 class Formatter : public IStrategy
 {
 public:
@@ -21,15 +23,15 @@ public:
 		return s1 + " " + s2 + "!";
 	}
 };
-
+//-------------------------------------------------------------------------------------------------
 void hello_world(const IStrategy & strategy)
 {
 	std::cout << strategy.format("Hello", "world") << std::endl;
 }
-
+//-------------------------------------------------------------------------------------------------
 int main()
 {
 	hello_world( Formatter() );
 	return 0;
 }
-
+//-------------------------------------------------------------------------------------------------
