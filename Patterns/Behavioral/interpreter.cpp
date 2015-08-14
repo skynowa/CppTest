d0ff94560a824029d04f6d68a4e61003ff9cffbd
skynowa <skynowa@gmail.com>
2015-08-14 16:27:10 +0300

    Patterns: interpreter - update

--- Patterns/Behavioral/interpreter.cpp
+++ Patterns/Behavioral/interpreter.cpp
@@ -1,41 +1,54 @@
 /*
-Interpreter processes commands and performs actions based on its input. 
+Interpreter processes commands and performs actions based on its input.
 Here we create a simple command language consisting of single characters.
 */
+
 
 #include <string>
 #include <iostream>
 #include <stdexcept>
 
+//-------------------------------------------------------------------------------------------------
 class Interpreter
 {
 public:
-	template<typename It>
-	void run(It from, It to)
+	template<typename T>
+	void run(T from, T to)
 	{
-		for(It i=from; i!=to; ++i)
-		{
-			switch(*i)
-			{
-			case 'h': std::cout << "Hello";		break;
-			case ' ': std::cout << ' ';			break;
-			case 'w': std::cout << "world";		break;
-			case '!': std::cout << '!';			break;
-			case 'n': std::cout << std::endl;	break;
-			default:  throw std::runtime_error("Unknown command");
+		for (T i = from; i != to; ++ i) {
+			switch (*i) {
+			case 'h':
+				std::cout << "Hello";
+				break;
+			case ' ':
+				std::cout << ' ';
+				break;
+			case 'w':
+				std::cout << "world";
+				break;
+			case '!':
+				std::cout << '!';
+				break;
+			case 'n':
+				std::cout << std::endl;
+				break;
+			default:
+				throw std::runtime_error("Unknown command");
 			}
-		}
+		} // for
 	}
 };
-
-void hello_world(const std::string & script)
+//-------------------------------------------------------------------------------------------------
+void hello_world(const std::string &script)
 {
-	Interpreter().run(script.begin(), script.end());
+	Interpreter interpreter;
+	interpreter.run(script.begin(), script.end());
 }
-
+//-------------------------------------------------------------------------------------------------
 int main()
 {
 	hello_world("h w!n");
+
 	return 0;
 }
-
+//-------------------------------------------------------------------------------------------------
