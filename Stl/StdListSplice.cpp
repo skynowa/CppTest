a646026097baeacdeddce719c45e131bc00bdcf6
skynowa <skynowa@gmail.com>
2015-08-28 11:09:19 +0300

    Stl: StdListSplice - add

---
+++ Stl/StdListSplice.cpp
@@ -0,0 +1,43 @@
+ /*
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include <string>
+#include <iostream>
+#include <list>
+#include <assert.h>
+
+//-------------------------------------------------------------------------------------------------
+#define TRACE_VAR(v) \
+    #v ": " << (v)
+//-------------------------------------------------------------------------------------------------
+int main(int argsNum, char **args)
+{
+	std::list<int> a {1, 2, 3, 4, 5, 6, 7, 8};
+	std::list<int> b {5, 6, 7, 8};
+
+	a.splice(a.end(), b);
+
+	std::cout << "a contains:";
+	for (auto &it : a )
+		std::cout << ' ' << it;
+	std::cout << '\n';
+
+	std::cout << "b contains:";
+	for (auto &it : b)
+		std::cout << ' ' << it;
+	std::cout << '\n';
+
+	return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+a contains: 1 2 3 4 5 6 7 8 5 6 7 8
+b contains:
+
+#endif
