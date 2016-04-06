3cf8e9e2649871abcbfe94f4dbaee05c9400aa74
skynowa <skynowa@gmail.com>
2016-04-06 10:18:02 +0300

    STL: update

--- Stl/set_difference.cpp
+++ Stl/set_difference.cpp
@@ -1,8 +1,7 @@
-// set_symmetric_difference example
-#include <iostream>     // std::cout
-#include <algorithm>    // std::set_symmetric_difference, std::sort
-#include <vector>       // std::vector
-
+#include <iostream>
+#include <algorithm>
+#include <vector>
+//-------------------------------------------------------------------------------------------------
 int main ()
 {
     int first[]  = {5, 10, 15, 20, 25, 100};
@@ -21,10 +20,17 @@ int main ()
     std::cout << "Difference has " << v.size() << " elements:\n";
 
     for (auto it : v) {
-        std::cout << ' ' << *it;
+        std::cout << ' ' << it;
     }
 
     std::cout << '\n';
 
     return 0;
 }
+//-------------------------------------------------------------------------------------------------
+#if 0
+
+5 15 25 100
+
+#endif
+//-------------------------------------------------------------------------------------------------
---
+++ Stl/set_symmetric_difference.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+//-------------------------------------------------------------------------------------------------
+int main ()
+{
+    std::map<std::string, int> a
+    {
+        {"a", 1},
+        {"b", 2},
+        {"c", 3},
+        {"d", 4}
+    };
+
+    std::map<std::string, int> b
+    {
+        {"a", 1},
+        {"b", 2},
+        {"c", 3}
+    };
+
+    std::map<std::string, int> diff;
+
+    std::set_symmetric_difference(
+        a.begin(), a.end(),
+        b.begin(), b.end(),
+        std::inserter(diff, diff.end()));
+
+    std::cout << "diff.size(): " << diff.size() << std::endl;
+
+    for (auto it : diff) {
+        std::cout << it.first << ", " << it.second << std::endl;
+    }
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+#if 0
+
+diff.size(): 1
+d, 4
+
+#endif
+//-------------------------------------------------------------------------------------------------
