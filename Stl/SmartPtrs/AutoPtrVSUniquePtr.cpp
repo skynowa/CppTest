018e2a19048a55def99261655e9e148e92d100a9
skynowa <skynowa@gmail.com>
2016-11-04 15:03:23 +0200

    Stl: SmartPtrs - add

--- Stl/SmartPtrs/AutoPtrVSUniquePtr.cpp
+++ Stl/SmartPtrs/AutoPtrVSUniquePtr.cpp
@@ -62,7 +62,10 @@ a.get(): {0, 0}
  *
  * - std::unique_ptr - can be stored in containers
  * - unique_ptr can handle arrays correctly (it will call delete[], while auto_ptr will attempt to call delete
+ * - права владения ресурсов уходят в y_ptr и x_ptr начинает указывать на null pointer
+ *   (сделает элемент вектора невалидным)
  * - В отличии от auto_ptr, unique_ptr запрещает копирование
+ *   Изменение прав владения ресурсом осуществляется с помощью вспомогательной функции std::move
  * - The boost::shared_ptr template makes it easy to handle multiple pointers to the same object,
  *   and the object is only deleted after the last reference to it went out of scope.
  */
