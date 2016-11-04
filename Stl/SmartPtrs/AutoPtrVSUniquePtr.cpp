a45082ea7af2ac0a4397c9d0fe327076afabcc26
skynowa <skynowa@gmail.com>
2016-11-04 15:18:14 +0200

    Stl: SmartPtrs - update

--- Stl/SmartPtrs/AutoPtrVSUniquePtr.cpp
+++ Stl/SmartPtrs/AutoPtrVSUniquePtr.cpp
@@ -6,6 +6,7 @@
 
 #include "../../StdTest.h"
 #include <memory>
+#include <vector>
 
 
 //-------------------------------------------------------------------------------------------------
@@ -41,7 +42,46 @@ int main(int argsNum, char **args)
 		std::cout << std::endl;
 	}
 
-    return 0;
+	// std::vector< std::auto_ptr<int> >
+	{
+		std::cout << "std::vector< std::auto_ptr<int> >" << std::endl;
+
+		std::vector< std::auto_ptr<int> > vec;
+		vec.push_back( std::auto_ptr<int>(new int(10)) );
+		vec.push_back( std::auto_ptr<int>(new int(11)) );
+		vec.push_back( std::auto_ptr<int>(new int(12)) );
+
+		std::auto_ptr<int> tmp = vec[1];
+
+		std::cout << _xTRACE_PTR(vec[0].get()) << std::endl;
+		std::cout << _xTRACE_PTR(vec[1].get()) << std::endl;
+		std::cout << _xTRACE_PTR(vec[2].get()) << std::endl;
+		std::cout << std::endl;
+	}
+
+	// std::vector< std::unique_ptr<int> >
+	{
+		std::cout << "std::vector< std::unique_ptr<int> >" << std::endl;
+
+		std::vector< std::unique_ptr<int> > vec;
+		vec.push_back( std::unique_ptr<int>(new int(10)) );
+		vec.push_back( std::unique_ptr<int>(new int(11)) );
+		vec.push_back( std::unique_ptr<int>(new int(12)) );
+
+	#if 0
+		// error: use of deleted function ‘std::unique_ptr<_Tp, _Dp>::unique_ptr(const std::unique_ptr<_Tp, _Dp>&) [with _Tp = int; _Dp = std::default_delete<int>]’
+		std::unique_ptr<int> tmp = vec[1];
+	#else
+		std::unique_ptr<int> tmp = std::move(vec[1]);
+	#endif
+
+		std::cout << _xTRACE_PTR(vec[0].get()) << std::endl;
+		std::cout << _xTRACE_PTR(vec[1].get()) << std::endl;
+		std::cout << _xTRACE_PTR(vec[2].get()) << std::endl;
+		std::cout << std::endl;
+	}
+
+	return 0;
 }
 //-------------------------------------------------------------------------------------------------
 
@@ -56,7 +96,18 @@ std::unique_ptr
 a.get(): {0, 0}
 b.get(): {0x55e445c75c30, 10}
 
+std::vector<std::auto_ptr<int> >
+vec[0].get(): {0x55c0e2d06c30, 10}
+vec[1].get(): {0, 0}
+vec[2].get(): {0x55c0e2d06c50, 12}
+
+std::vector< std::unique_ptr<int> >
+vec[0].get(): {0x5586b76d3c50, 10}
+vec[1].get(): {0, 0}
+vec[2].get(): {0x5586b76d3c30, 12}
+
 #endif
+
 
 /**
  * FAQ
