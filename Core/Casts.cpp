c7c764bb575ad0f6cdb49527ef5616c5b7b03e0f
skynowa <skynowa@gmail.com>
2016-12-29 15:10:28 +0200

    Core: Casts - add

---
+++ Core/Casts.cpp
@@ -0,0 +1,32 @@
+/**
+ * \file  Casts.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+
+//-------------------------------------------------------------------------------------------------
+int main(int argsNum, char **args)
+{
+    int try_avail = 78.4;
+
+    double drv1 = try_avail         + 0.001;
+    double drv2 = (double)try_avail + 0.001;
+
+    std::cout << drv1 << std::endl;
+    std::cout << drv2 << std::endl;
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+78.001
+78.001
+
+#endif
+
+
