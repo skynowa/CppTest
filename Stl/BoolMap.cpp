f4c0e691fb678107097e14214516e33c495c549a
skynowa <skynowa@gmail.com>
2017-01-16 12:06:05 +0200

    Stl: BoolMap - add

---
+++ Stl/BoolMap.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+#include <map>
+
+//-------------------------------------------------------------------------------------------------
+int main(int argsNum, char **args)
+{
+	std::map<std::string, bool> m;
+	m["A"] = true;
+	m["B"] = false;
+	m["C"] = true;
+
+	for (auto &it_m : m) {
+		std::cout << "{" << it_m.first << "," << it_m.second << "}" << std::endl;
+	}
+
+	std::cout << "{Z," << m["Z"] << "}" << std::endl;
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+{A,1}
+{B,0}
+{C,1}
+{Z,0}
+
+#endif
+
