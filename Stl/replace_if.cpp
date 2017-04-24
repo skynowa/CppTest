bfaa992c02953df230479cc0961e43a3bcf0fb16
skynowa <skynowa@gmail.com>
2017-04-24 08:55:35 +0300

    STL: replace_if - add

---
+++ Stl/replace_if.cpp
@@ -0,0 +1,35 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+#include <algorithm>
+#include <cctype>
+//-------------------------------------------------------------------------------------------------
+bool
+functor(const char a_it)
+{
+	return !std::isalnum( static_cast<int>(a_it) );
+}
+//-------------------------------------------------------------------------------------------------
+int main(int argsNum, char **args)
+{
+	std::string value = "Su tton (London,UK)";
+
+	value.erase(std::remove_if(value.begin(), value.end(), functor), value.end());
+
+	std::cout << STD_TRACE_VAR(value) << std::endl;
+
+	return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+value: SuttonLondonUK
+
+#endif
+
