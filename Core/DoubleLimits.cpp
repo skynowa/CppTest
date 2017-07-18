f37cf4fbccea703d24e02f312b6766a1cd9489b8
skynowa <skynowa@gmail.com>
2017-07-18 10:52:42 +0300

    Core: DoubleLimits - add

---
+++ Core/DoubleLimits.cpp
@@ -0,0 +1,32 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+
+#include <limits>
+
+//-------------------------------------------------------------------------------------------------
+int main(int argsNum, char **args)
+{
+	std::cout << STD_TRACE_VAR(std::numeric_limits<float>::max()) << std::endl;
+	std::cout << STD_TRACE_VAR(std::numeric_limits<float>::min()) << std::endl;
+	std::cout << STD_TRACE_VAR(std::numeric_limits<float>::infinity()) << std::endl;
+
+	float dRv = strtod("2037362412.80", nullptr);
+
+    std::cout << STD_TRACE_VAR(dRv) << std::endl;
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
+
