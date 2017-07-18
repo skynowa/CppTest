c8adda9e4f9c31b58b1ee5dcf42792a80f733509
skynowa <skynowa@gmail.com>
2017-07-18 10:53:03 +0300

    Network: mount

---
+++ Network/mount.cpp
@@ -0,0 +1,54 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+
+#include <sys/mount.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <string>
+
+using namespace std;
+
+//-------------------------------------------------------------------------------------------------
+int main(int argsNum, char **args)
+{
+  string host = "192.168.12.200";
+  string src = "\\\\homes";
+  string dst = "~/tmp/jail";
+  string fstype = "cifs";
+
+  string all_string = "unc=" + src + ",ip=" + host + ",username=skynowa,password=1978abccbA";
+
+  printf("src: %s\n", src.c_str());
+
+  if( -1 == mount(src.c_str(), dst.c_str(), fstype.c_str(), MS_MGC_VAL | MS_SILENT , all_string.c_str()))
+  {
+      printf("mount failed with error: %s\n",strerror(errno));
+  }
+  else
+      printf("mount success!\n");
+
+  if( umount2(dst.c_str(), MNT_FORCE) < 0 )
+  {
+      printf("unmount failed with error: %s\n",strerror(errno));
+  }
+  else
+      printf("unmount success!\n");
+
+
+  return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
+
