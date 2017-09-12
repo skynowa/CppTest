971b49701841a49181ee9df86cead043938ef1cb
skynowa <skynowa@gmail.com>
2017-09-12 11:54:53 +0300

    STL: forward

--- Stl/utility/forward.cpp
+++ Stl/utility/forward.cpp
@@ -1,28 +1,44 @@
-// forward example
-#include <utility>      // std::forward
-#include <iostream>     // std::cout
+/**
+ * \file  forward.cpp
+ * \brief
+ */
 
+
+#include "../../StdTest.h"
+#include <utility>
+#include <iostream>
+//-------------------------------------------------------------------------------------------------
 // function with lvalue and rvalue reference overloads:
 void overloaded (const int& x) {std::cout << "[lvalue]";}
-void overloaded (int&& x) {std::cout << "[rvalue]";}
-
+void overloaded (int&& x)      {std::cout << "[rvalue]";}
+//-------------------------------------------------------------------------------------------------
 // function template taking rvalue reference to deduced type:
 template <class T> void
-fn (T&& x) {
-  overloaded (x);                   // always an lvalue
-  overloaded (std::forward<T>(x));  // rvalue if argument is rvalue
+fn(T&& x)
+{
+    overloaded (x);                   // always an lvalue
+    overloaded (std::forward<T>(x));  // rvalue if argument is rvalue
 }
+//-------------------------------------------------------------------------------------------------
+int main(int argsNum, char **args)
+{
+    int a;
 
-int main () {
-  int a;
+    std::cout << "calling fn with lvalue: ";
+    fn(a);
+    std::cout << '\n';
 
-  std::cout << "calling fn with lvalue: ";
-  fn (a);
-  std::cout << '\n';
+    std::cout << "calling fn with rvalue: ";
+    fn(0);
+    std::cout << '\n';
 
-  std::cout << "calling fn with rvalue: ";
-  fn (0);
-  std::cout << '\n';
-
-  return 0;
+    return 0;
 }
+//-------------------------------------------------------------------------------------------------
+
+#if OUTPUT
+
+calling fn with lvalue: [lvalue][lvalue]
+calling fn with rvalue: [lvalue][rvalue]
+
+#endif
