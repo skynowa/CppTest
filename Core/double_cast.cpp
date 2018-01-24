215b0af47aa64fa99f1cbbd8df119c2c3e00f1b1
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-01-24 17:10:16 +0200

    Core: double_cast - add

---
+++ Core/double_cast.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include <StdTest.h>
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	{
+	    double     d = -0.15;
+	    signed int i = static_cast<signed int>(d);
+
+	    std::cout << STD_TRACE_VAR2(d, i) << std::endl;
+    }
+
+    {
+	    double     d = 0.25;
+	    signed int i = static_cast<signed int>(d);
+
+	    std::cout << STD_TRACE_VAR2(d, i) << std::endl;
+	}
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+d: -0.15, i: 0
+d:  0.25, i: 0
+
+#endif
+
