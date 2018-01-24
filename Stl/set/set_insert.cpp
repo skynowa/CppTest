6270743923fbef08722ecd5b499604f4408f0365
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-01-24 10:29:06 +0200

    set: set_insert

---
+++ Stl/set/set_insert.cpp
@@ -0,0 +1,40 @@
+/**
+ * \file  set_insert.cpp
+ * \brief
+ */
+
+
+#include <StdTest.h>
+#include <set>
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	std::set<int> s;
+
+	auto it1 = s.insert(1);
+	std::cout << STD_TRACE_VAR(it1.second) << std::endl;
+
+	auto it2 = s.insert(2);
+	std::cout << STD_TRACE_VAR(it2.second) << std::endl;
+
+	auto it3 = s.insert(2);
+	std::cout << STD_TRACE_VAR(it3.second) << std::endl;
+
+	auto it4 = s.insert(3);
+	std::cout << STD_TRACE_VAR(it4.second) << std::endl;
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+it1.second: 1
+it2.second: 1
+it3.second: 0
+it4.second: 1
+
+#endif
+
