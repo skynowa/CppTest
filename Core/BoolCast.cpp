11174adf264ecd71754fc221fe7c375027c99a05
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-01-29 18:39:50 +0200

    Core: BoolCast - add

---
+++ Core/BoolCast.cpp
@@ -0,0 +1,52 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include <StdTest.h>
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	// int
+	{
+		std::cout << STD_TRACE_VAR(static_cast<bool>(-10)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(-0)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(0)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(20)) << std::endl;
+		std::cout << std::endl;
+	}
+
+	// double
+	{
+		std::cout << STD_TRACE_VAR(static_cast<bool>(-10.0)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(-0.0)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(-0.01)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(0.0)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(0.001)) << std::endl;
+		std::cout << STD_TRACE_VAR(static_cast<bool>(20.0)) << std::endl;
+		std::cout << std::endl;
+	}
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+static_cast<bool>(-10): 1
+static_cast<bool>(-0): 0
+static_cast<bool>(0): 0
+static_cast<bool>(20): 1
+
+static_cast<bool>(-10.0): 1
+static_cast<bool>(-0.0): 0
+static_cast<bool>(-0.01): 1
+static_cast<bool>(0.0): 0
+static_cast<bool>(0.001): 1
+static_cast<bool>(20.0): 1
+
+#endif
+
