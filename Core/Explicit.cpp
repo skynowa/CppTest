ebdefc5489fa8ce13d54f214eb58004b69771a9a
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-02-02 12:39:31 +0200

    Core: Explicit - add

---
+++ Core/Explicit.cpp
@@ -0,0 +1,48 @@
+/**
+ * \file  Explicit.cpp
+ * \brief
+ */
+
+
+#include <StdTest.h>
+
+//-------------------------------------------------------------------------------------------------
+struct A
+{
+	operator bool() const { return true; }
+};
+
+struct B
+{
+	explicit operator bool() const { return true; }
+};
+
+struct C
+{
+	explicit operator bool() const { return true; }
+};
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	A a;
+	std::cout << STD_TRACE_VAR(a) << std::endl;
+
+	B b;
+	//std::cout << STD_TRACE_VAR(b) << std::endl;
+
+	C c;
+	std::cout << STD_TRACE_VAR(static_cast<bool>(c)) << std::endl;
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+a: 1
+no known conversion for argument 2 from 'B' to 'int'
+static_cast<bool>(c): 1
+
+#endif
+
