9dda8cf96ddc8250ca38035589289892f32feadb
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-02-02 12:39:56 +0200

    IpcMt: atomic_flag - add

---
+++ IpcMt/C++11/atomic_flag.cpp
@@ -0,0 +1,52 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include <StdTest.h>
+#include <thread>
+#include <vector>
+#include <iostream>
+#include <atomic>
+
+//-------------------------------------------------------------------------------------------------
+std::atomic_flag lock = ATOMIC_FLAG_INIT;
+//-------------------------------------------------------------------------------------------------
+void f(const int n)
+{
+    for (int cnt = 0; cnt < 3; ++cnt) {
+        while (lock.test_and_set(std::memory_order_acquire)) {
+             ;
+        }
+
+        std::cout << "Output from thread " << STD_TRACE_VAR(n) << std::endl;
+        std::cout << "----------" << std::endl;
+
+        lock.clear(std::memory_order_release);
+    }
+}
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+    std::vector<std::thread> v;
+
+    for (int n = 0; n < 4; ++ n) {
+        v.emplace_back(f, n);
+    }
+
+    for (auto& t : v) {
+        t.join();
+    }
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
+
