3aea41a192589de511489445d42e909a23411c4b
skynowa <skynowa@gmail.com>
2018-04-24 21:41:39 +0300

    Core: Gcc_LambdaLeaky - add

---
+++ Core/Gcc_LambdaLeaky.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file  main.cpp
+ * \brief https://www.youtube.com/watch?v=S9_mYmvO4Ow
+ */
+
+
+#include "../StdTest.h"
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	auto l = [val = 2]()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+
+		return val;
+	};
+
+	std::cout << STD_TRACE_VAR(l.__val) << std::endl;
+
+	l.__val = 4;
+
+	std::cout << STD_TRACE_VAR(l.__val) << std::endl;
+
+    return l();
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+l.__val: 2
+l.__val: 4
+operator()
+
+#endif
+
