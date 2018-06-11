f57720bcc699bae39b8f8ca1b8ebd1b28b7c99f9
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-06-11 14:09:42 +0300

    eclipse.sh

---
+++ eclipse.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# \file  eclipse.sh
+# \brief build project
+#
+
+
+# vars
+DIR_XLIB="../CppTest"
+DIR_BUILD="../CppTest_eclipse"
+
+
+# build
+
+# Eclipse versions:
+#   - 3.2 Callisto
+#   - 3.3 Europa
+#   - 3.4 Ganymede
+#   - 3.5 Galileo
+#   - 3.6 Helios
+#   - 3.7 Indigo
+#   - 4.2 Juno
+#   - 4.3 Kepler
+#   - 4.4 Luna
+#   - 4.5 Mars
+
+mkdir -p $DIR_BUILD
+cd $DIR_BUILD
+
+cmake \
+	-G"Eclipse CDT4 - Unix Makefiles" \
+	-D_ECLIPSE_VERSION=4.3 \
+	-DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE \
+	$DIR_XLIB
