7ec5e218b34d87c760c5f944bf8dc1ed2adf48b9
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-09-03 12:32:03 +0300

    GoTo: add

---
+++ Core/GoTo.cpp
@@ -0,0 +1,77 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+#include "../Stl.h"
+//-------------------------------------------------------------------------------------------------
+class Widget
+{
+public:
+	Widget() = default;
+	~Widget() = default;
+
+	bool show()
+	{
+		return false;
+	}
+
+	bool isValid() const
+	{
+		return true;
+	}
+
+	void cleanup()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+};
+//-------------------------------------------------------------------------------------------------
+std::string
+runWidget()
+{
+    std::string error;
+
+    Widget a;
+    if ( !a.show() ) {
+		error = "a_error";
+		goto exit;
+    }
+
+    Widget b;
+    if ( !b.show() ) {
+		error = "b_error";
+		goto exit;
+    }
+
+    // ...
+
+exit:
+	if ( a.isValid() ) {
+		a.cleanup();
+	}
+
+	if ( b.isValid() ) {
+		b.cleanup();
+	}
+
+	return error;
+}
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	::runWidget();
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+cleanup
+cleanup
+
+#endif
