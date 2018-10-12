9ba2a768100ac65dcb9395f9cfd5af96fcfcad5c
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-10-12 10:24:10 +0300

    Templates: VariadicTemplates3 - add

---
+++ Templates/VariadicTemplates3.cpp
@@ -0,0 +1,46 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+#include "../Stl.h"
+
+//-------------------------------------------------------------------------------------------------
+template<typename ... Param>
+std::vector<std::string>
+toString(const Param & ... a_param)
+{
+	const auto toString_impl = [](const auto &t) -> std::string
+	{
+		std::stringstream ss;
+		ss << t;
+
+		return ss.str();
+	};
+
+	return { toString_impl(a_param)... };
+}
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	const std::vector<std::string> vec = ::toString("hello", 1, 5.3, "bob");
+
+    for (auto &it : vec) {
+		std::cout << STD_TRACE_VAR(it) << std::endl;
+	}
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+it: hello
+it: 1
+it: 5.3
+it: bob
+
+#endif
