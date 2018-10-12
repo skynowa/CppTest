8aff65699cf54c0e51f33364ab936be6674c18dc
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-10-12 10:23:52 +0300

    Stl: ListSort - add

---
+++ Stl/List/ListSort.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include "../StdTest.h"
+#include "../Stl.h"
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	std::list<std::size_t> mylist;
+	mylist.push_back(10);
+	mylist.push_back(30);
+	mylist.push_back(0);
+
+	mylist.sort(
+		[](const std::size_t a_it1, const std::size_t a_it2) -> bool
+		{
+			return a_it1 < a_it2;
+		});
+
+	for (auto it : mylist) {
+		s1td::cout << ',' << *it;
+	}
+
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+10,30,0 -> 0,10,30
+
+#endif
