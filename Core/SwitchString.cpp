5ef1ec5b065640850e3e686f67efa952cf418479
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-04-29 23:09:21 +0300

    SwitchString.cpp - implementation

--- Core/SwitchString.cpp
+++ Core/SwitchString.cpp
@@ -1,48 +1,103 @@
 /**
  * \file  main.cpp
- * \brief
+ * \brief https://github.com/rioki/rex/blob/master/strex.h#L71
  */
 
 
 #include <StdStream.h>
 #include <StdTest.h>
 #include <Stl.h>
-#include <iostream>
-#include <functional>
-#include <string>
 //-------------------------------------------------------------------------------------------------
-constexpr unsigned int
-hash(const char *str, int h = 0)
+constexpr std::size_t
+hash(
+	const char        *a_str,
+	const std::size_t  a_h = {}
+)
 {
-    return !str[h] ? 5381 : (hash(str, h + 1) * 33) ^ str[h];
+	if (a_str == nullptr) {
+		return {};
+	}
+
+    return !a_str[a_h] ? 5381 : (hash(a_str, a_h + 1) * 33) ^ a_str[a_h];
+}
+//-------------------------------------------------------------------------------------------------
+std::size_t
+hash(
+	const std::string &a_str
+)
+{
+	return ::hash( a_str.c_str() );
 }
 //-------------------------------------------------------------------------------------------------
 void
-switch_test(const std::string& name)
+ifString_test(
+	const std::string &a_value
+)
 {
-	std::cout << name << " -> ";
-
-    switch (::hash(name.c_str()))
-    {
-    case ::hash("Alice"):
-        std::cout << "Alice" << std::endl;
+    if      (a_value == "aaaaa") {
+		std::cout << "aaaaa" << std::endl;
+	}
+	else if (a_value == "bbbbb") {
+		std::cout << "bbbbb" << std::endl;
+	}
+	else if (a_value == "ccccc") {
+		std::cout << "ccccc" << std::endl;
+	}
+	else {
+		std::cout << "[Unknown]: " << ::hash(a_value) << std::endl;
+	}
+}
+//-------------------------------------------------------------------------------------------------
+void
+switchString_test(
+	const char *a_value
+)
+{
+    switch (::hash(a_value)) {
+    case ::hash("aaaaa"):
+        std::cout << "aaaaa" << std::endl;
         break;
-    case ::hash("Bob"):
-        std::cout << "Bob" << std::endl;
+    case ::hash("bbbbb"):
+        std::cout << "bbbbb" << std::endl;
         break;
-    case ::hash("Charlie"):
-		std::cout << "Charlie" << std::endl;
+    case ::hash("ccccc"):
+		std::cout << "ccccc" << std::endl;
         break;
     default:
-        std::cout << "[Unknown]" << std::endl;
+        std::cout << "[Unknown]: " << ::hash(a_value) << std::endl;
         break;
     }
 }
 //-------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
-    ::switch_test("alice");
-    ::switch_test("Alice");
+	{
+	#if 1
+		std::cout << "::: ifString_test :::" << std::endl;
+
+		::ifString_test("aaaaa");
+		::ifString_test("bbbbb");
+		::ifString_test("ccccc");
+		::ifString_test("xxxxx");
+		::ifString_test("");
+		::ifString_test({});	// nullptr
+	#endif
+	}
+
+	std::cout << std::endl;
+
+	{
+	#if 1
+		std::cout << "::: switchString_test :::" << std::endl;
+
+		::switchString_test("aaaaa");
+		::switchString_test("bbbbb");
+		::switchString_test("ccccc");
+		::switchString_test("xxxxx");
+		::switchString_test("");
+		::switchString_test(nullptr);
+	#endif
+	}
 
     return EXIT_SUCCESS;
 }
