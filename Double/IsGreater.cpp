2542bc2ae409180a6fdb27cdda57fc5d9da396ea
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-08-20 00:04:27 +0300

    IsGreater.cpp - add

---
+++ Double/IsGreater.cpp
@@ -0,0 +1,34 @@
+/**
+ * \file  main.cpp
+ * \brief
+ *
+ * Returns whether x is greater than y.
+ */
+
+
+#include <StdStream.h>
+#include <StdTest.h>
+#include <Stl.h>
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	{
+		const double d    {0.000000001};
+		const double test {0.0}
+
+		const bool bRv = std::isgreater(, 0.0);
+
+    	std::cout << TRACE_VAR3(d, test, bRv) << std::endl;
+	}
+
+    return EXIT_SUCCESS;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
