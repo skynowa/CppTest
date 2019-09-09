97d62aca1ea1e3118d5d2c3e572e90c78f6b56c8
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-09-09 15:48:19 +0300

    ProgramOptions.cpp - review

--- Libs/Boost/ProgramOptions.cpp
+++ Libs/Boost/ProgramOptions.cpp
@@ -13,49 +13,49 @@
 #include <iterator>
 #include <iostream>
 
-using namespace boost::program_options;
-
+namespace po = boost::program_options;
+//-------------------------------------------------------------------------------------------------
 void to_cout(const std::vector<std::string> &v)
 {
 	std::copy(v.begin(), v.end(), std::ostream_iterator<std::string>{std::cout, "\n"});
 }
-
+//-------------------------------------------------------------------------------------------------
 int main(int argc, const char *argv[])
 {
 	try {
-		int age;
+		int age {};
 
-		options_description desc{"Options"};
+		po::options_description desc{"Options"};
 		desc.add_options()
 			("help,h", "Help screen")
-			("pi",     value<float>()->implicit_value(3.14f), "Pi")
-			("age",    value<int>(&age), "Age")
-			("phone",  value<std::vector<std::string>>()->multitoken()->zero_tokens()->composing(), "Phone")
+			("pi",     po::value<float>()->implicit_value(3.14f), "Pi")
+			("age",    po::value<int>(&age), "Age")
+			("phone",  po::value<std::vector<std::string>>()->multitoken()->zero_tokens()->composing(), "Phone")
 			("unreg",  "Unrecognized options");
 
-		command_line_parser parser{argc, argv};
+		po::command_line_parser parser{argc, argv};
 		parser.options(desc).allow_unregistered().style(
-			command_line_style::default_style |
-			command_line_style::allow_slash_for_short);
-		parsed_options parsed_options = parser.run();
+			po::command_line_style::default_style |
+			po::command_line_style::allow_slash_for_short);
+		po::parsed_options parsed_options = parser.run();
 
-		variables_map vm;
-		store(parsed_options, vm);
-		notify(vm);
+		po::variables_map vm;
+		po::store(parsed_options, vm);
+		po::notify(vm);
 
-		if (vm.count("help"))
+		if      (vm.count("help"))
 			std::cout << desc << '\n';
 		else if (vm.count("age"))
 			std::cout << "Age: " << age << '\n';
 		else if (vm.count("phone"))
 			to_cout(vm["phone"].as<std::vector<std::string>>());
 		else if (vm.count("unreg"))
-			to_cout(collect_unrecognized(parsed_options.options,
-										 exclude_positional));
+			to_cout(collect_unrecognized(parsed_options.options, po::exclude_positional));
 		else if (vm.count("pi"))
 			std::cout << "Pi: " << vm["pi"].as<float>() << '\n';
 	}
-	catch (const error &ex) {
+	catch (const po::error &ex) {
 		std::cerr << ex.what() << '\n';
 	}
 }
+//-------------------------------------------------------------------------------------------------
