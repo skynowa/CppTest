eb62b6610cfba8635473a736898a7b4755f4ed04
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-01-14 15:44:03 +0200

    move

---
+++ Core/C++17/StructuredBindingsRef.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+// Driver code
+int main( )
+{
+	Point p = { 1,2 };
+
+	// Structure binding
+	auto &[x_coord, y_coord] = p;
+
+    // cout << "type: " << typeid( decltype([x_coord, y_coord]) ).name() << endl;
+
+	x_coord = 7;
+	y_coord = 8;
+
+	cout << "X Coordinate : " << x_coord << endl;
+	cout << "Y Coordinate : " << y_coord << endl;
+
+  	cout << "p.x : " << p.x << endl;
+	cout << "p.y : " << p.y << endl;
+
+	return 0;
+}
--- Templates/Readme.md
+++ Templates/Readme.md
@@ -2,3 +2,4 @@
 
 
 https://www.youtube.com/watch?v=-PKyagrtIfs&feature=youtu.be
+https://www.youtube.com/watch?v=ULX_VTkMvf8
