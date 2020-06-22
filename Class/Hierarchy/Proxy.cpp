1d3edaee78b0d6f2ddc8405e038094debb1b56e5
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-06-22 09:41:07 +0300

    Proxy.cpp - add

---
+++ Class/Hierarchy/Proxy.cpp
@@ -0,0 +1,84 @@
+/**
+ * \file  main.cpp
+ * \brief
+ *
+ * COMMENTS
+ */
+
+
+#include <StdStream.h>
+#include <StdTest.h>
+#include <Stl.h>
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+//-------------------------------------------------------------------------------------------------
+class A
+{
+public:
+    A() = default;
+    virtual ~A() = default;
+
+    virtual void foo(int i)
+    {
+        std::cout << "A::" << __FUNCTION__ << ": " << i << std::endl;
+    }
+};
+//-------------------------------------------------------------------------------------------------
+class Proxy :
+	public A
+{
+public:
+    Proxy() = default;
+
+    void foo(int i) override
+    {
+        std::cout << "Proxy::" << __FUNCTION__ << ": " << i << std::endl;
+    }
+};
+//-------------------------------------------------------------------------------------------------
+class ProxyNo :
+	public A
+{
+public:
+    void foo(int i) override
+    {
+        std::cout << "ProxyNo::" << __FUNCTION__ << ": " << i << std::endl;
+    }
+};
+//-------------------------------------------------------------------------------------------------
+class ProxyYes :
+	public Proxy
+{
+public:
+    void foo(int i) override
+    {
+        std::cout << "ProxyYes::" << __FUNCTION__ << ": " << i << std::endl;
+    }
+};
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+    std::map<int, A *> objects;
+    objects[0] = new ProxyNo();
+    objects[1] = new ProxyYes();
+
+    for (auto &[key, value] : objects) {
+        value->foo(key);
+
+		delete value; value = nullptr;
+    }
+
+    return EXIT_SUCCESS;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+ProxyNo::foo: 0
+ProxyYes::foo: 1
+
+#endif
