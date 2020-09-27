37998be3b8c474eec1e70b80487c350886f89a86
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-27 13:55:55 +0300

    eclipse.sh - Eclipse version - add

--- eclipse.sh
+++ eclipse.sh
@@ -9,6 +9,7 @@
 PROJECT_NAME=$(basename ${PWD})
 DIR_PROJECT="../${PROJECT_NAME}"
 DIR_BUILD="../${PROJECT_NAME}_eclipse"
+ECLIPSE_VERSION="4.17" # 2020-09
 
 # prepare
 mkdir -p $DIR_BUILD
@@ -17,4 +18,5 @@ cd $DIR_BUILD
 # build
 cmake \
 	-G"Eclipse CDT4 - Unix Makefiles" \
+	-D_ECLIPSE_VERSION=${ECLIPSE_VERSION} \
 	$DIR_PROJECT
