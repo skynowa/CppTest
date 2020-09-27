4341b726a57d096ef73827265671526656cd0654
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-27 13:49:59 +0300

    CMakeLists.txt - update

--- eclipse.sh
+++ eclipse.sh
@@ -1,34 +1,28 @@
 #!/usr/bin/env bash
 #
 # \file  eclipse.sh
-# \brief build project
+# \brief CMake configure project
 #
 
 
 # vars
-DIR_XLIB="../CppTest"
-DIR_BUILD="../CppTest_eclipse"
+PROJECT_NAME=$(basename ${PWD})
+DIR_PROJECT="../${PROJECT_NAME}"
+DIR_BUILD="../${PROJECT_NAME}_eclipse"
+ECLIPSE_VERSION="4.17" # 2020-09
 
+echo "PROJECT_NAME: ${PROJECT_NAME}"
+echo "DIR_PROJECT: ${DIR_PROJECT}"
+echo "DIR_BUILD: ${DIR_BUILD}"
+echo "ECLIPSE_VERSION: ${ECLIPSE_VERSION}"
 
-# build
-
-# Eclipse versions:
-#   - 3.2 Callisto
-#   - 3.3 Europa
-#   - 3.4 Ganymede
-#   - 3.5 Galileo
-#   - 3.6 Helios
-#   - 3.7 Indigo
-#   - 4.2 Juno
-#   - 4.3 Kepler
-#   - 4.4 Luna
-#   - 4.5 Mars
-
+# prepare
 mkdir -p $DIR_BUILD
 cd $DIR_BUILD
 
+# build
 cmake \
 	-G"Eclipse CDT4 - Unix Makefiles" \
-	-D_ECLIPSE_VERSION=4.3 \
+	-D_ECLIPSE_VERSION=${ECLIPSE_VERSION} \
 	-DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE \
-	$DIR_XLIB
+	$DIR_PROJECT
