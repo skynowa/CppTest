15a76d52d7ee3f71d578a275f28ec4387022f600
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-11-10 05:15:41 +0200

    transform.cpp - add

---
+++ Stl/Algos/transform.cpp
@@ -0,0 +1,45 @@
+/**
+ * \file  main.cpp
+ * \brief
+ *
+ * COMMENTS
+ */
+
+
+#include <StdStream.h>
+#include <StdTest.h>
+#include <Stl.h>
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+    const auto valuesIn  = std::vector<int>{1, 2, 3, 4, 5};
+    auto       valuesOut = std::vector<double>{};
+
+    std::transform(valuesIn.cbegin(), valuesIn.cend(),
+        std::back_inserter(valuesOut),
+        [](const  int number)
+		{
+			return number * 1.2;
+		});
+
+    std::for_each(valuesOut.cbegin(), valuesOut.cend(),
+        [](const double number)
+		{
+			std::cout << TRACE_VAR(number) << std::endl;
+		});
+
+    return EXIT_SUCCESS;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+number: 1.2
+number: 2.4
+number: 3.6
+number: 4.8
+number: 6
+
+#endif
