0b0fe143a70b31b92903fc46056b46d5ab24bab1
skynowa <skynowa@gmail.com>
2021-01-27 12:10:38 +0200

    slice.cpp - add

---
+++ Stl/Vector/slice.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+vector<T>
+slice(
+    const vector<T> &v,
+    const size_t     begin,
+    const size_t     end
+)
+{
+	auto first = v.cbegin() + begin;
+	auto last  = v.cbegin() + end + 1;
+
+	return {first, last};
+}
+
+int main()
+{
+	const vector<int> vec {7,6,2,4,1,9,10,15,17};
+
+	vector<int> subVec = slice(vec, 3, 6);
+
+	cout << xSTD_TRACE(subVec) <<endl;
+
+	return 0;
+}
+
+#if 0
+
+4 1 9 10
+
+#endif
