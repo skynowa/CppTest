608dda7f65f441ccfd7fa310fb90cfb677bcd4b8
skynowa <skynowa@gmail.com>
2021-04-09 23:56:28 +0300

    W32DLL.cpp - update

--- Windows/RegDLL/W32DLL/W32DLL.cpp
+++ Windows/RegDLL/W32DLL/W32DLL.cpp
@@ -6,10 +6,12 @@
 
 #include "stdafx.h"
 //-------------------------------------------------------------------------------------------------
-BOOL APIENTRY DllMain( HANDLE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY
+DllMain(
+    HANDLE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
 {
     return TRUE;
 }
