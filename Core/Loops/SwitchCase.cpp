ed551bb5d3572311257e41130ace0a0bc8e11bbd
skynowa <skynowa@gmail.com>
2021-04-20 00:12:28 +0300

    SwitchCase.cpp - update

--- Core/Loops/SwitchCase.cpp
+++ Core/Loops/SwitchCase.cpp
@@ -1,7 +1,7 @@
 /**
- * \file  SwitchCase.cpp.cpp
- * \brief Modern switch / case
- */
+* \file  SwitchCase.cpp.cpp
+* \brief Modern switch / case
+*/
 
 
 #include <StdStream.h>
@@ -13,29 +13,29 @@ main()
 {
     const int ints[] {-1, 0, 2, 3, 4, 6, 7, 200};
 
-	for (const auto it_int : ints) {
-		std::cout << it_int << " -> ";
+    for (const auto it_int : ints) {
+        std::cout << it_int << " -> ";
 
-		switch (it_int) {
-		case 0 ... 3:
-			std::cout << "[0 ... 3]" << std::endl;
-			break;
-		case 4 ... 5:
-			std::cout << "[4 ... 5]" << std::endl;
-			break;
-		case 6:
-			std::cout << "[6]" << std::endl;
-			break;
-		case 7 ... INT_MAX:
-			std::cout << "[7 ... INT_MAX]" << std::endl;
-			break;
-		default:
-			std::cout << "[default]" << std::endl;
-			break;
-		}
-	}
+        switch (it_int) {
+        case 0 ... 3:
+            std::cout << "[0 ... 3]" << std::endl;
+            break;
+        case 4 ... 5:
+            std::cout << "[4 ... 5]" << std::endl;
+            break;
+        case 6:
+            std::cout << "[6]" << std::endl;
+            break;
+        case 7 ... INT_MAX:
+            std::cout << "[7 ... INT_MAX]" << std::endl;
+            break;
+        default:
+            std::cout << "[default]" << std::endl;
+            break;
+        }
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 //---------------------------------------------------------------------------
 
