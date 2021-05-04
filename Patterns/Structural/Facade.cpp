bd9970416fed85729264135adebdc5b177c883d6
skynowa <skynowa@gmail.com>
2021-05-04 10:44:03 +0300

    Facade.cpp - done

--- Patterns/Structural/Facade.cpp
+++ Patterns/Structural/Facade.cpp
@@ -1,8 +1,6 @@
 /**
  * \file  Facade.cpp
- * \brief
- *
- * \todo
+ * \brief Wrap / simplify complex behaviour
  *
  * Facade hides a lot of complex behaviour behind a simple, single interface.
  * It puts a shop-front on an otherwise complex set of classes.
@@ -13,44 +11,52 @@
 #include <StdTest.h>
 #include <Stl.h>
 //--------------------------------------------------------------------------------------------------
-class HelloWorld
+class IHelloWorld
 {
 public:
-	virtual ~HelloWorld() { }
-	virtual std::string hello()=0;
-	virtual std::string world()=0;
-	virtual void output(const std::string&)=0;
-	virtual void endl()=0;
-};
+	virtual ~IHelloWorld() = default;
 
-class HelloWorldImpl : public HelloWorld
+	virtual std::string hello() = 0;
+	virtual std::string world() = 0;
+
+	virtual void output(const std::string &) = 0;
+	virtual void endl() = 0;
+};
+//--------------------------------------------------------------------------------------------------
+class HelloWorldImpl :
+	public IHelloWorld
 {
 public:
 	std::string hello() { return "Hello"; }
 	std::string world() { return "world"; }
+
 	void output(const std::string & str)
 	{
 		std::cout << str;
 	}
+
 	void endl()
 	{
 		std::cout << std::endl;
 	}
 };
-
-void hello_world(HelloWorld & hw)
-{
-	hw.output(hw.hello());
-	hw.output(" ");
-	hw.output(hw.world());
-	hw.output("!");
-	hw.endl();
-}
-
+//--------------------------------------------------------------------------------------------------
 int main()
 {
 	HelloWorldImpl hw;
-	hello_world(hw);
+
+	hw.output( hw.hello() );
+	hw.output(" ");
+	hw.output( hw.world() );
+	hw.output("!");
+	hw.endl();
+
 	return 0;
 }
 //--------------------------------------------------------------------------------------------------
+
+#if OUTPUT
+
+Hello world!
+
+#endif
