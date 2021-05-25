665a9af0d6d279255d5f19f86257d9e627e8e392
skynowa <skynowa@gmail.com>
2021-05-25 14:47:05 +0300

    MoveRef.cpp - done

--- Core/Move/MoveRef.cpp
+++ Core/Move/MoveRef.cpp
@@ -1,5 +1,5 @@
 /**
- * \file
+ * \file  MoveRef.cpp
  * \brief
  *
  * \todo
@@ -9,26 +9,44 @@
 #include <StdStream.h>
 #include <StdTest.h>
 #include <Stl.h>
-
 //-------------------------------------------------------------------------------------------------
-struct A
+class A
 {
-    std::string value;
-
+public:
     A() :
-        value() {std::cout << "A()" << std::endl;}
-    A(const std::string &a_value):
-        value(a_value) {std::cout << "A(std::string a_value)" << std::endl; }
+        _value()
+    {
+        std::cout << "A()" << std::endl;
+    }
+
+    explicit A(const std::string &a_value):
+        _value(a_value)
+    {
+        std::cout << "A(std::string a_value)" << std::endl;
+    }
 
     A(const A &a) :
-        value(a.value) {std::cout << "A(const A &)" << std::endl;}
-    A(A &&a) :
-        value(move(a.value)) {std::cout << "A(A &&)" << std::endl;}
+        _value(a._value)
+    {
+        std::cout << "A(const A &)" << std::endl;
+    }
+
+    explicit A(A &&a) :
+        _value(move(a._value))
+    {
+        std::cout << "A(A &&)" << std::endl;
+    }
 
     // A & operator = (const A &) {std::cout << "A & operator = (const A &)" << std::endl;}
     // A & operator = (A &&) {std::cout << "A & operator = (A &&)" << std::endl;}
 
-    ~A() {std::cout << "~A()" << std::endl;}
+    ~A()
+    {
+        std::cout << "~A()" << std::endl;
+    }
+
+public:
+    std::string _value;
 };
 //-------------------------------------------------------------------------------------------------
 int main(int, char **)
@@ -47,24 +65,38 @@ int main(int, char **)
     // v.push_back(a2);
     // v.push_back(a3);
 
-    // std::cout << "std::vector: " << v[0].value << std::endl;
+    // std::cout << "std::vector: " << v[0]._value << std::endl;
     std::cout << "\n***** Test *****" << std::endl;
 
     for (auto &&it : v) {
-        auto test = move(it.value);
+        auto test = move(it._value);
 
-        // std::cout << test.value << std::endl;
-        // std::cout << it.value << std::endl;
-        std::cout << it.value << (it.value.empty() ? "<moved>" : it.value) << std::endl;
+        // std::cout << test._value << std::endl;
+        // std::cout << it._value << std::endl;
+        std::cout << it._value << (it._value.empty() ? "<moved>" : it._value) << std::endl;
     }
 
-    std::cout << TRACE_VAR(a1.value) << std::endl;
+    std::cout << TRACE_VAR(a1._value) << std::endl;
 
     // std::cout <<"size = " <<  v.size() << std::endl;
     // std::cout << v[0] << std::endl;
     // std::cout << v[1] << std::endl;
     // std::cout << v[2] << std::endl;
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 //-------------------------------------------------------------------------------------------------
+
+#if OUTPUT
+
+A(std::string a_value)
+A(const A &)
+
+***** Test *****
+<moved>
+a1._value: STRING
+~A()
+~A()
+
+
+#endif
