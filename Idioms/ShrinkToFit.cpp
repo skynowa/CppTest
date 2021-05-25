6dc203d90fa4726c35b5c0d51c9116fa98424d50
skynowa <skynowa@gmail.com>
2021-05-26 00:02:08 +0300

    ShrinkToFit.cpp - done

--- Idioms/ShrinkToFit.cpp
+++ Idioms/ShrinkToFit.cpp
@@ -2,6 +2,13 @@
  * \file  ShrinkToFit.cpp
  * \brief Minimize the capacity of a container just enough to hold existing range.
  *
+ * Standard library containers often allocate more memory than the actual number of elements in them.
+ * Such a policy results in an optimization of saving some allocations when a container grows in size.
+ * On the other hand, when size of the container reduces, there is often leftover capacity
+ * in the container. The leftover capacity of the container can be unnecessary wastage of memory
+ * resources. Shrink-to-fit idiom has been developed to reduce the extra capacity to a minimum
+ * required capacity and thereby saving memory resources.
+ *
  * In C++11 some containers declare such idiom as function shrink_to_fit(),
  * e.g. vector, deque, basic_string. shrink_to_fit() is a non-binding request
  * to reduce capacity() to size()
