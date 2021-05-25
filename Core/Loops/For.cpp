eceb29638c0c8c78bfcbf02b895153edc7644bd5
skynowa <skynowa@gmail.com>
2021-05-25 14:14:36 +0300

    For.cpp - done

--- Core/Loops/For.cpp
+++ Core/Loops/For.cpp
@@ -1,31 +1,33 @@
 /**
- * \file
+ * \file  For.cpp
  * \brief
- *
- * \todo
  */
 
 
-//---------------------------------------------------------------------------
-#include <string>
-#include <iostream>
-#include <assert.h>
+#include <StdStream.h>
+#include <StdTest.h>
+#include <Stl.h>
 //---------------------------------------------------------------------------
 bool
-trace(const std::string &csPoint) {
-    std::cout << csPoint;
+trace(const std::string &a_point)
+{
+    std::cout << a_point;
 
     return true;
 }
 //---------------------------------------------------------------------------
 int main(int, char **)
 {
-    int g = 0;
-    for (trace("1"); trace("2"); trace("3")) {
-        std::cout << " [block]" << std::endl;
+    int i = 0;
 
-        ++ g;
-        if (5 == g) break;
+    for (trace("1,"); trace("2"); trace("3,")) {
+        std::cout << " {" << TRACE_VAR(i) << "}" << std::endl;
+
+        ++ i;
+
+        if (i == 5) {
+            break;
+        }
     }
 
     return EXIT_SUCCESS;
@@ -35,10 +37,10 @@ int main(int, char **)
 
 #if OUTPUT
 
-12 [block]
-32 [block]
-32 [block]
-32 [block]
-32 [block]
+1,2 {i: 0}
+3,2 {i: 1}
+3,2 {i: 2}
+3,2 {i: 3}
+3,2 {i: 4}
 
 #endif
