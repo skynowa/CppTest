596d120342cc0342235a2a1c579013539cd6de1b
skynowa <skynowa@gmail.com>
2021-05-25 13:33:57 +0300

    SizeofUnicodes.cpp - done

--- Core/Char/SizeofUnicodes.cpp
+++ Core/Char/SizeofUnicodes.cpp
@@ -1,38 +1,30 @@
 /**
- * \file
+ * \file  SizeofUnicodes.cpp
  * \brief
  *
  * \todo
  */
 
 
-//---------------------------------------------------------------------------
-#include <string>
-#include <iostream>
-#include <assert.h>
-//---------------------------------------------------------------------------
+#include <StdStream.h>
+#include <StdTest.h>
+#include <Stl.h>
+//--------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
-    std::cout << "sizeof(_T('|0')) = " << sizeof(L'\0') << std::endl;
-    std::cout << "sizeof(_T('0'))  = " << sizeof(L'0')  << std::endl;
-    std::cout << "sizeof(0)        = " << sizeof(0)        << std::endl;
+    std::cout << TRACE_VAR(sizeof(L'\0')) << std::endl;
+    std::cout << TRACE_VAR(sizeof(L'0'))  << std::endl;
+    std::cout << TRACE_VAR(sizeof(0))     << std::endl;
 
     return EXIT_SUCCESS;
 }
-//---------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 
 
 #if OUTPUT
 
-// Ansi
-    sizeof(_T('|0')) = 1
-    sizeof(_T('0'))  = 1
-    sizeof(_T(0))    = 4
-    sizeof(0)        = 4
-
-// Unicode
-    sizeof(_T('|0')) = 2
-    sizeof(_T('0'))  = 2
-    sizeof(0)        = 4
+sizeof(_T('|0')) = 4
+sizeof(_T('0'))  = 4
+sizeof(0)        = 4
 
 #endif
