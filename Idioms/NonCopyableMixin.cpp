f722e29c6c73c10454ad5b97004b2dac29083e42
skynowa <skynowa@gmail.com>
2021-05-26 13:03:49 +0300

    NonCopyableMixin.cpp - done

---
+++ Idioms/NonCopyableMixin.cpp
@@ -0,0 +1,43 @@
+/**
+ * \file  NonCopyableMixin.cpp
+ * \brief To prevent objects of a class from being copy-constructed or assigned to each other
+ */
+
+
+#include <StdStream.h>
+#include <StdTest.h>
+#include <Stl.h>
+//--------------------------------------------------------------------------------------------------
+class NonCopyable
+{
+public:
+	NonCopyable (const NonCopyable &) = delete;
+	NonCopyable & operator = (const NonCopyable &) = delete;
+
+protected:
+	NonCopyable () = default;
+	~NonCopyable () = default;
+		///< Protected non-virtual destructor
+};
+//-------------------------------------------------------------------------------------------------
+class CantCopy :
+	private NonCopyable
+{
+};
+//--------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+
+
+    // std::cout << TRACE_VAR("") << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//--------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
