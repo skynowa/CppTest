a80ebbf5b76b081cb7211c2b00a7e439a8baac92
skynowa <skynowa@gmail.com>
2021-08-03 21:38:23 +0300

    DependencyInjection.cpp - add todo(s)

---
+++ Idioms/DependencyInjection.cpp
@@ -0,0 +1,30 @@
+/**
+ * \file  DependencyInjection.cpp
+ * \brief
+ *
+ * \todo
+ *
+ * https://github.com/boost-ext/di
+ */
+
+
+#include <StdStream.h>
+#include <StdTest.h>
+#include <Stl.h>
+//--------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+
+
+    // std::cout << TRACE_VAR("") << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//--------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
