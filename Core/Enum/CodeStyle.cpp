e248a00fea354adea4a5b0fd0b992f3936c57928
skynowa <skynowa@gmail.com>
2021-12-07 17:08:18 +0200

    CodeStyle.cpp [33+/18-] - review

--- Core/Enum/CodeStyle.cpp
+++ Core/Enum/CodeStyle.cpp
@@ -8,40 +8,54 @@
 #include <StdTest.h>
 #include <Stl.h>
 //---------------------------------------------------------------------------
-namespace NOpenMode
+namespace open_mode
 {
 
 enum EType
 {
     Read1 = 0,
     Read2 = 1,
-    READ3 = 2
+    READ3 = 2,
+    Max   = READ3 + 1
+};
+
+enum class /* struct */ Alpha
+{
+    a   = 0,
+    b   = 1,
+    c   = 2,
+    max = c + 1
 };
 
 };
 //---------------------------------------------------------------------------
-class AClass
+class A
 {
 public:
-    enum EOpenMode {
+    enum OpenMode
+    {
         omRead         = 0,
         OpenMode_Read  = 1,
         OPEN_MODE_READ = 2
     };
 
-    AClass()
-    {
-    }
+    A() = default;
 };
 //---------------------------------------------------------------------------
 void
-foo1(NOpenMode::EType mode)
+foo1(open_mode::EType mode)
 {
     std::cout << "mode: " << mode << std::endl;
 }
 //---------------------------------------------------------------------------
 void
-foo2(AClass::EOpenMode mode)
+printAlpha(open_mode::Alpha mode)
+{
+    std::cout << "mode: " << static_cast<int>(mode) << std::endl;
+}
+//---------------------------------------------------------------------------
+void
+foo3(A::OpenMode mode)
 {
     std::cout << "mode: " << mode << std::endl;
 }
@@ -49,19 +63,20 @@ foo2(AClass::EOpenMode mode)
 int main(int, char **)
 {
     {
-        std::cout << NOpenMode::Read1 << std::endl;
-        std::cout << NOpenMode::Read2 << std::endl;
-        std::cout << NOpenMode::READ3 << std::endl;
+        std::cout << open_mode::Read1 << std::endl;
+        std::cout << open_mode::Read2 << std::endl;
+        std::cout << open_mode::READ3 << std::endl;
+        std::cout << open_mode::Max   << std::endl;
         std::cout << std::endl;
     }
 
     {
-        AClass::EOpenMode mode = AClass::OPEN_MODE_READ;
+        A::OpenMode mode = A::OPEN_MODE_READ;
 
-        std::cout << mode                   << std::endl;
-        std::cout << AClass::omRead         << std::endl;
-        std::cout << AClass::OpenMode_Read  << std::endl;
-        std::cout << AClass::OPEN_MODE_READ << std::endl;
+        std::cout << mode              << std::endl;
+        std::cout << A::omRead         << std::endl;
+        std::cout << A::OpenMode_Read  << std::endl;
+        std::cout << A::OPEN_MODE_READ << std::endl;
         std::cout << std::endl;
     }
 
@@ -75,11 +90,11 @@ int main(int, char **)
 0
 1
 2
+3
 
 2
 0
 1
 2
-
 
 #endif
