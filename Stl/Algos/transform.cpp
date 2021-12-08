a3b0e5f663350a2f19cab54ab58acc39787259fc
skynowa <skynowa@gmail.com>
2021-12-08 23:55:23 +0200

    transform.cpp [44+/17-] - impl

--- Stl/Algos/transform.cpp
+++ Stl/Algos/transform.cpp
@@ -1,8 +1,6 @@
 /**
  * \file  transform.cpp
  * \brief
- *
- * \todo
  */
 
 
@@ -12,21 +10,50 @@
 //-------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
-    const auto valuesIn  = std::vector<int>{1, 2, 3, 4, 5};
-    auto       valuesOut = std::vector<double>{};
+    {
+        std::cout << "\nSample #1" << std::endl;
 
-    std::transform(valuesIn.cbegin(), valuesIn.cend(),
-        std::back_inserter(valuesOut),
-        [](const  int number)
-		{
-			return number * 1.2;
-		});
+        const std::vector<int> valuesIn {1, 2, 3, 4, 5};
+        std::vector<double>    valuesOut;
 
-    std::for_each(valuesOut.cbegin(), valuesOut.cend(),
-        [](const double number)
-		{
-			std::cout << TRACE_VAR(number) << std::endl;
-		});
+        std::transform(valuesIn.cbegin(), valuesIn.cend(),
+            std::back_inserter(valuesOut),
+            [](const int a_num) -> int
+            {
+                std::cout << TRACE_VAR(a_num) << std::endl;
+
+                return a_num * 1.2;
+            });
+    }
+
+    {
+        std::cout << "\nSample #2" << std::endl;
+
+        std::vector<int> valuesIn {1, 2, 3, 4, 5};
+
+        std::transform(valuesIn.begin(), valuesIn.end(),
+            valuesIn.begin(),
+            [](const int a_num) -> int
+            {
+                std::cout << TRACE_VAR(a_num) << std::endl;
+
+                return a_num * 1.2;
+            });
+    }
+
+    {
+        std::cout << "\nSample #3" << std::endl;
+
+        const std::vector<int> valuesIn {1, 2, 3, 4, 5};
+
+        std::for_each(valuesIn.cbegin(), valuesIn.cend(),
+            [](const int a_num) -> int
+            {
+                std::cout << TRACE_VAR(a_num) << std::endl;
+
+                return a_num * 1.2;
+            });
+    }
 
     return EXIT_SUCCESS;
 }
