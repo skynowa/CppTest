003bea3b0069305b230b5584a67de683cf1fc03d
skynowa <skynowa@gmail.com>
2022-05-13 20:42:06 +0300

    ListRemoveIf.cpp [31+/20-] - done

--- Stl/List/ListRemoveIf.cpp
+++ Stl/List/ListRemoveIf.cpp
@@ -1,34 +1,45 @@
 /**
- * \file
+ * \file  ListRemoveIf.cpp
  * \brief
  *
- * \todo
+ * Removes from the container all the elements for which Predicate pred returns true.
+ * This calls the destructor of these objects and reduces the container size by
+ * the number of elements removed.
  */
 
 
-#include <iostream>
-#include <list>
-#include <algorithm>
-
-int main()
+#include <StdStream/StdStream.h>
+#include <StdTest/StdTest.h>
+#include <Stl.h>
+//--------------------------------------------------------------------------------------------------
+int main(int, char **)
 {
-	// Create a list and initialize it with 7 elements
 	std::list<int> listOfInts( { 2, 3, 4, 6, 4, 9, 1, 2, 8, 9, 4, 6, 2 });
 
+	std::cout << "Before: " << TRACE_VAR(listOfInts.size()) << std::endl;
+
 	// Remove only first occurrence of element with value 4
+	auto pred = [](const int & val) -> bool
+	{
+		return (val >= 2 && val < 5);
+	};
 
-	listOfInts.remove_if([](const int & val) {
-		if(val >= 2 && val < 5)
-			return true;
-		else
-			return false;
-	});
+	listOfInts.remove_if(pred);
 
-	std::cout << "size: " << listOfInts.size() << std::endl << std::endl;
+	std::cout << "After: " << TRACE_VAR(listOfInts.size()) << std::endl;
+	std::cout << listOfInts << std::endl;
+	std::cout << std::endl;
 
-	for (const auto &val : listOfInts) {
-		std::cout << val << std::endl;
-	}
-
-	return 0;
+	return EXIT_SUCCESS;
 }
+//--------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+Before: listOfInts.size(): 13
+After:  listOfInts.size(): 6
+
+std::list (size=6): {6,9,1,8,9,6}
+
+#endif
