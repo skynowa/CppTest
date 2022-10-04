e730952f40bf1b33f376c2cf116b2ca517d20103
skynowa <skynowa@gmail.com>
2022-10-04 12:37:53 +0300

    CompInt.cpp [3+/11-] - review

--- Stl/MultiMap/CompInt.cpp
+++ Stl/MultiMap/CompInt.cpp
@@ -8,23 +8,15 @@
 #include <StdTest/StdTest.h>
 #include <Stl.h>
 //-------------------------------------------------------------------------------------------------
-struct CmpInt
-{
-    bool operator() (const int key1, const int key2) const
-    {
-        return key1 < key2;
-    }
-};
-
-using int_mmap_t = std::multimap<int, int, ::CmpInt>;
+using int_mmap_t = std::multimap<int, int, std::less<int>>;
 //--------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
     const ::int_mmap_t intMap
     {
         {2, 2},
-        {1, 1},
-        {0, 0}
+        {0, 0},
+        {1, 1}
     };
 
     for (const auto &[it_key, it_value] : intMap) {
