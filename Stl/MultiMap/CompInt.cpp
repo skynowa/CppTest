2d731dd0b46db6289af60b9bf3e1acfecd22a851
skynowa <skynowa@gmail.com>
2022-10-04 12:46:32 +0300

    CompInt.cpp [41+/11-] - impl

--- Stl/MultiMap/CompInt.cpp
+++ Stl/MultiMap/CompInt.cpp
@@ -8,19 +8,41 @@
 #include <StdTest/StdTest.h>
 #include <Stl.h>
 //-------------------------------------------------------------------------------------------------
-using int_mmap_t = std::multimap<int, int, std::less<int>>;
+using int_mmap_less_t    = std::multimap<int, int, std::less<int>>;
+using int_mmap_greater_t = std::multimap<int, int, std::greater<int>>;
 //--------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
-    const ::int_mmap_t intMap
     {
-        {2, 2},
-        {0, 0},
-        {1, 1}
-    };
+        const ::int_mmap_less_t intMap
+        {
+            {3, 200},
+            {1, 400},
+            {4, 999},
+            {2, 300}
+        };
 
-    for (const auto &[it_key, it_value] : intMap) {
-        std::cout << "{" << it_key << ", " << it_value << "}" << std::endl;
+        std::cout << "int_mmap_less_t:" << std::endl;
+
+        for (const auto &[it_key, it_value] : intMap) {
+            std::cout << "{" << it_key << ", " << it_value << "}" << std::endl;
+        }
+    }
+
+    {
+        const ::int_mmap_greater_t intMap
+        {
+            {3, 200},
+            {1, 400},
+            {4, 999},
+            {2, 300}
+        };
+
+        std::cout << "\nint_mmap_greater_t:" << std::endl;
+
+        for (const auto &[it_key, it_value] : intMap) {
+            std::cout << "{" << it_key << ", " << it_value << "}" << std::endl;
+        }
     }
 
     return EXIT_SUCCESS;
@@ -30,8 +52,16 @@ int main(int, char **)
 
 #if OUTPUT
 
-{0, 0}
-{1, 1}
-{2, 2}
+int_mmap_less_t:
+{1, 400}
+{2, 300}
+{3, 200}
+{4, 999}
+
+int_mmap_greater_t:
+{4, 999}
+{3, 200}
+{2, 300}
+{1, 400}
 
 #endif
