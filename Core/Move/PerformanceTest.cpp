fc8379bcf53f3dcf5d8a113dde50f4a371a30d25
skynowa <skynowa@gmail.com>
2023-01-19 04:04:32 +0200

    PerformanceTest.cpp (+2) [164+/168-] - impl

--- Core/Move/PerformanceTest.cpp
+++ Core/Move/PerformanceTest.cpp
@@ -12,154 +12,110 @@
 #include <StdStream/StdStream.h>
 #include <StdTest/StdTest.h>
 #include <Stl.h>
-//--------------------------------------------------------------------------------------------------
-#define OPTION_COPY 1
-#define OPTION_MOVE 1
-//--------------------------------------------------------------------------------------------------
-struct A
-{
-    A() = default;
-    ~A() = default;
 
-    explicit A(const std::string &name) :
-        _name(name)
-    {
-    }
-
-    // explicit - n/a
-	A(const char *name) :
-        _name(name)
-    {
-    }
-
-#if OPTION_COPY
-	A(const A& ref)
-	{
-		// STD_TRACE_FUNC;
-
-		_name = ref._name;
-	}
-
-	A& operator=(const A& ref)
-	{
-		// STD_TRACE_FUNC;
-
-		_name = ref._name;
-		return *this;
-	}
-#endif
-
-#if OPTION_MOVE
-    A(A&& ref) noexcept
-    {
-        // STD_TRACE_FUNC;
-
-        _name = std::move(ref._name);
-    }
-
-    A& operator=(A&& ref) noexcept
-    {
-        // STD_TRACE_FUNC;
-
-        _name = std::move(ref._name);
-
-        return *this;
-    }
-#endif
-
-    std::string _name;
-};
+#include <Idioms/RuleOf5.h>
 //--------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
-    const std::vector<A> vec =
-    {
-        "k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
-        "pzSJD'kfsngv;adojgn;aojngear;on",
-        ";pksaMfkojnsg;'lkeng'eaoignea'oig",
-        "ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
-        "sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-        "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
-        "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-        "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
-        "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-        "vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
-        "asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
-        "gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
-        "gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver",
-        "k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
-        "pzSJD'kfsngv;adojgn;aojngear;on",
-        ";pksaMfkojnsg;'lkeng'eaoignea'oig",
-        "ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
-        "sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-        "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
-        "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-        "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
-        "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-        "vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
-        "asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
-        "gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
-        "gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver",
-        "k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
-        "pzSJD'kfsngv;adojgn;aojngear;on",
-        ";pksaMfkojnsg;'lkeng'eaoignea'oig",
-        "ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
-        "sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-        "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
-        "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-        "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
-        "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-        "vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
-        "asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
-        "gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
-        "gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver",
-        "k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
-        "pzSJD'kfsngv;adojgn;aojngear;on",
-        ";pksaMfkojnsg;'lkeng'eaoignea'oig",
-        "ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
-        "sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-        "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
-        "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-        "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
-        "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-        "vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
-        "asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
-        "gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
-        "gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver"
-    };
+	const std::vector<RuleOf5> vec =
+	{
+		"k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
+		"pzSJD'kfsngv;adojgn;aojngear;on",
+		";pksaMfkojnsg;'lkeng'eaoignea'oig",
+		"ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
+		"sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
+		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+		"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+		"dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+		"cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+		"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+		"vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
+		"asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
+		"gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
+		"gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver",
+		"k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
+		"pzSJD'kfsngv;adojgn;aojngear;on",
+		";pksaMfkojnsg;'lkeng'eaoignea'oig",
+		"ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
+		"sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
+		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+		"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+		"dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+		"cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+		"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+		"vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
+		"asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
+		"gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
+		"gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver",
+		"k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
+		"pzSJD'kfsngv;adojgn;aojngear;on",
+		";pksaMfkojnsg;'lkeng'eaoignea'oig",
+		"ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
+		"sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
+		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+		"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+		"dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+		"cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+		"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+		"vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
+		"asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
+		"gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
+		"gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver",
+		"k;jzxdxfjndfajk;bvmkl;adklm;dkl;m",
+		"pzSJD'kfsngv;adojgn;aojngear;on",
+		";pksaMfkojnsg;'lkeng'eaoignea'oig",
+		"ckjhbnlxkjdhbnlkzdjbnklnjbjnbjnbjnjnnjbjnkxcbknxcbnjkxc",
+		"sdpfjgbosdjosidjgosigjdfiojsodijbsoijsoibijbjrjoiojbjbjiijrtijjrhjsjbisibjsijbjpsibsiojbosigbisobiosjbojisfg",
+		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+		"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+		"dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+		"cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+		"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+		"vbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbsdddddddddddddddddddddddd",
+		"asdfasdfasdfasdfasdfsadfsadfsadfsadfsadfsadfasdfasdfasdfasdfasdfsdfasdfa",
+		"gdgdgdgdgdgdgdgdgdgdggdggdgdgdgdgduydgiuaysdfguasdyfguasdfuysadfgasduiyfgsafguysadfasugyf",
+		"gaegarefahfwaiefhaiuoyhfairufhaiufhaeifuhrefiarhfiuhfiuahfiuahgierugheiuheulihafuhvaiuvvvvvvvavavavver"
+	};
 
-    std::uint64_t count {};
+	std::uint64_t count {};
 
-    auto start_time = std::chrono::high_resolution_clock::now();
+	const auto start_time = std::chrono::high_resolution_clock::now();
 
-    while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start_time).count() < 60)
-    {
-        auto tmp_vec = vec;
+	while (std::chrono::duration_cast<
+		std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start_time).count() < 60)
+	{
+		auto tmp_vec = vec;
 
-        while ( !tmp_vec.empty() ) {
-            tmp_vec.erase( tmp_vec.cbegin() );
-        }
+		while ( !tmp_vec.empty() ) {
+			tmp_vec.erase( tmp_vec.cbegin() );
+		}
 
-        ++ count;
-    }
+		++ count;
+	}
 
-#if OPTION_MOVE
-    const std::string label = "Move - ";
-#else
-    const std::string label = "Copy - ";
-#endif
+	const std::string labelCopy =
+	#if RULEOF5_OPTION_COPY
+		"Copy";
+	#else
+		"";
+	#endif
 
-    std::cout << label << STD_TRACE_VAR(count) << std::endl;
+	const std::string labelMove =
+	#if RULEOF5_OPTION_MOVE
+		"Move";
+	#else
+		"";
+	#endif
 
-    return EXIT_SUCCESS;
+	std::cout
+		<< "[" << labelCopy << " / " << labelMove << "] "
+		<< STD_TRACE_VAR(count) << std::endl;
+
+	return EXIT_SUCCESS;
 }
 //--------------------------------------------------------------------------------------------------
 
--- Idioms/RuleOf5.h
+++ Idioms/RuleOf5.h
@@ -3,6 +3,10 @@
  * \brief The rule of three/five/zero
  *
  * \see   https://en.cppreference.com/w/cpp/language/rule_of_three
+ *
+ * Because the presence of a user-defined destructor, copy-constructor, or copy-assignment operator
+ * prevents implicit definition of the move constructor and the move assignment operator, any class
+ * for which move semantics are desirable, has to declare all five special member functions
  */
 
 
@@ -10,86 +14,122 @@
 #include <StdTest/StdTest.h>
 #include <Stl.h>
 //--------------------------------------------------------------------------------------------------
+#define RULEOF5_OPTION_LOG  0
+#define RULEOF5_OPTION_COPY 1
+#define RULEOF5_OPTION_MOVE 1
+//--------------------------------------------------------------------------------------------------
 class RuleOf5
 {
 public:
-///\name ctors, dtor
-///\{
 	RuleOf5();
+
+	explicit RuleOf5(const std::string &a_value) :
+		_value(a_value)
+	{
+	}
+
+	// explicit - n/a
+	RuleOf5(const char *a_value) :
+		_value(a_value)
+	{
+	}
+
+#if RULEOF5_OPTION_COPY
 	RuleOf5(const RuleOf5 &obj);
+	RuleOf5 & operator = (const RuleOf5 &obj);
+#endif
+
+#if RULEOF5_OPTION_MOVE
 	RuleOf5(RuleOf5 &&obj);
+	RuleOf5 & operator = (RuleOf5 &&obj);
+#endif
 
 	~RuleOf5();
-///\}
-
-///\name operators
-///\{
-	RuleOf5 & operator = (const RuleOf5 &obj);
-	RuleOf5 & operator = (RuleOf5 &&obj);
 
 	friend std::ostream & operator << (std::ostream &os, const RuleOf5 &obj);
-///\}
 
 private:
-	std::size_t i {};
+	std::string _value {};
 };
 //--------------------------------------------------------------------------------------------------
 RuleOf5::RuleOf5()
 {
+#if RULEOF5_OPTION_LOG
 	std::cout << "\t::: [Ctor] Default :::" << std::endl;
+#endif
 }
+//-------------------------------------------------------------------------------------------------
+
+#if RULEOF5_OPTION_COPY
 //--------------------------------------------------------------------------------------------------
 RuleOf5::RuleOf5(
 	const RuleOf5 &a_obj
 ) :
-	i{a_obj.i}
+	_value{a_obj._value}
 {
+#if RULEOF5_OPTION_LOG
 	std::cout << "\t::: [Ctor] Copy :::" << std::endl;
+#endif
 }
+//--------------------------------------------------------------------------------------------------
+RuleOf5 &
+RuleOf5::operator = (
+	const RuleOf5 &a_obj
+)
+{
+	if (this == &a_obj) {
+		return *this;
+	}
+
+#if RULEOF5_OPTION_LOG
+	std::cout << "\t::: [Op] Copy assignment :::" << std::endl;
+#endif
+
+	_value = a_obj._value;
+
+	return *this;
+}
+//-------------------------------------------------------------------------------------------------
+#endif
+
+#if RULEOF5_OPTION_MOVE
 //--------------------------------------------------------------------------------------------------
 RuleOf5::RuleOf5(
 	RuleOf5 &&a_obj
 ) :
-	i{ std::move(a_obj.i) }
+	_value{ std::move(a_obj._value) }
 {
+#if RULEOF5_OPTION_LOG
 	std::cout << "\t::: [Ctor] Move :::" << std::endl;
+#endif
 }
+//--------------------------------------------------------------------------------------------------
+RuleOf5 &
+RuleOf5::operator = (
+	RuleOf5 &&a_obj
+)
+{
+	if (this == &a_obj) {
+		return *this;
+	}
+
+#if RULEOF5_OPTION_LOG
+	std::cout << "\t::: [Op] Move assignment :::" << std::endl;
+#endif
+
+	_value = std::move(a_obj._value);
+
+	return *this;
+}
+//--------------------------------------------------------------------------------------------------
+#endif
+
 //--------------------------------------------------------------------------------------------------
 RuleOf5::~RuleOf5() /* final */
 {
+#if RULEOF5_OPTION_LOG
 	std::cout << "\t::: [Dtor] :::" << std::endl;
-}
-//--------------------------------------------------------------------------------------------------
-RuleOf5 &
-RuleOf5::operator = (
-	const RuleOf5 &a_obj
-)
-{
-	if (this == &a_obj) {
-		return *this;
-	}
-
-	std::cout << "\t::: [Op] Copy assignment :::" << std::endl;
-
-	i = a_obj.i;
-
-	return *this;
-}
-//--------------------------------------------------------------------------------------------------
-RuleOf5 &
-RuleOf5::operator = (
-	RuleOf5 &&a_obj
-)
-{
-	if (this == &a_obj) {
-		return *this;
-	}
-
-	std::cout << "\t::: [Op] Move assignment :::" << std::endl;
-
-	i = std::move(a_obj.i);
-
-	return *this;
+#endif
 }
 //--------------------------------------------------------------------------------------------------
 inline std::ostream &
@@ -98,6 +138,6 @@ operator << (
 	const RuleOf5 &a_obj
 )
 {
-	return out_os << "{" << a_obj.i << "}";
+	return out_os << "{" << a_obj._value << "}";
 }
 //-------------------------------------------------------------------------------------------------
