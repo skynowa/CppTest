bf0daed8ed5e40fd920cfadb237308433daaaf2f
skynowa <skynowa@gmail.com>
2023-01-20 20:45:04 +0200

    Rvo.cpp [28+] - impl

---
+++ Idioms/Rvo.cpp
@@ -0,0 +1,28 @@
+/**
+ * \file  main.cpp
+ * \brief
+ *
+ * \todo
+ */
+
+
+#include <StdStream/StdStream.h>
+#include <StdTest/StdTest.h>
+#include <Stl.h>
+//--------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+    // STD_TRACE_FUNC;
+
+    // std::cout << STD_TRACE_VAR("") << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//--------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
