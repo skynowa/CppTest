27f217c94114041a763b1e017edd6df96aa9d3f2
skynowa <skynowa@gmail.com>
2023-02-06 00:21:31 +0200

    Rvo.cpp [1+/3-] - Defines - fix

--- Idioms/Rvo.cpp
+++ Idioms/Rvo.cpp
@@ -19,9 +19,7 @@
 #include <StdTest/StdTest.h>
 #include <Stl.h>
 
-#define RULEOF5_OPTION_LOG  1
-#define RULEOF5_OPTION_COPY 1
-#define RULEOF5_OPTION_MOVE 1
+#define RULEOF5_OPTION_LOG 1
 
 #include <Idioms/Rule5.h>
 //--------------------------------------------------------------------------------------------------
