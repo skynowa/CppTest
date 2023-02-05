ea4b7385dda629518b6fc94571623ff9c4fb6c7f
skynowa <skynowa@gmail.com>
2023-02-06 01:46:20 +0200

    Move.cpp [73-] - dead(s)

--- Core/Move/Move.cpp
+++ Core/Move/Move.cpp
@@ -13,55 +13,6 @@
 #define RULEOF5_OPTION_LOG 1
 #include <Idioms/Rule3Move.h>
 //-------------------------------------------------------------------------------------------------
-class File
-{
-public:
-    explicit File(const char *filename)
-    {
-        _handle = std::fopen(filename, "r");
-        if (_handle == nullptr) {
-            throw std::runtime_error("blah blah blah");
-        }
-
-        std::cout << "File(const char *)" << std::endl;
-    }
-
-    File(const File &) = delete;
-
-    File(File &&that)
-    {
-        _handle      = that._handle;
-        that._handle = nullptr;
-
-        std::cout << "File(File &&)" << std::endl;
-    }
-
-    // Ops
-    File & operator = (const File &) = delete;
-
-    File & operator = (File &&that)
-    {
-        std::swap(_handle, that._handle);
-
-        std::cout << "operator = (File &&)" << std::endl;
-
-        return *this;
-    }
-
-    ~File()
-    {
-        if (_handle != nullptr) {
-            std::fclose(_handle);
-            _handle = nullptr;
-        }
-
-        std::cout << "~File()" << std::endl;
-    }
-
-private:
-    FILE *_handle {};
-};
-//-------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
     {
@@ -92,30 +43,6 @@ int main(int, char **)
 }
 //-------------------------------------------------------------------------------------------------
 
-
-#if OUTPUT
-
-::::: files.push_back('data1.txt') :::::
-File(const char *)
-File(File &&)
-~File()
-
-::::: files.push_back('data2.txt') :::::
-File(const char *)
-File(File &&)
-File(File &&)
-~File()
-~File()
-
-::::: files.erase() :::::
-operator = (File &&)
-~File()
-
-::::: files - dtor :::::
-~File()
-
-
-#endif
 
 #if OUTPUT
 
