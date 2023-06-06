a8664b278eadc20c8949aae1a726312891dded2e
skynowa <skynowa@gmail.com>
2023-06-06 21:57:26 +0300

    DependencyInversion.cpp [28+] - todo(s)

---
+++ Idioms/DependencyInversion.cpp
@@ -0,0 +1,28 @@
+/**
+ * \file  DependencyInversion.cpp
+ * \brief
+ *
+ * \todo
+ */
+
+
+#include <StdStream/StdStream.h>
+#include <StdTest/StdTest.h>
+#include <Stl.h>
+//--------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+
+
+    // std::cout << STD_TRACE_VAR("") << std::endl;
+
+    return EXIT_SUCCESS;
+}
+//--------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
