aab1965a740c9cb25f6c01ba390f43efc802522d
skynowa <skynowa@gmail.com>
2024-01-10 22:49:13 +0200

    Sort.cpp [14+/20-] - review

--- Stl/List/Sort.cpp
+++ Stl/List/Sort.cpp
@@ -1,39 +1,33 @@
 /**
  * \file  Sort.cpp
  * \brief
- *
- * \todo
  */
 
 
+#include <StdStream/StdStream.h>
 #include <StdTest/StdTest.h>
 #include <Stl.h>
-
 //-------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
-	std::list<std::size_t> mylist;
-	mylist.push_back(10);
-	mylist.push_back(30);
-	mylist.push_back(0);
+	std::list<int> items {10, 30, 0};
 
-	mylist.sort(
-		[](const std::size_t a_it1, const std::size_t a_it2) -> bool
-		{
-			return a_it1 < a_it2;
-		});
+	auto comp = [](const int a_it1, const int a_it2) -> bool
+	{
+		return a_it1 < a_it2;
+	};
 
-	for (auto &it : mylist) {
-		std::cout << ',' << it;
-	}
+	items.sort(comp);
 
-    return 0;
+	std::cout << items << std::endl;
+
+	return EXIT_SUCCESS;
 }
 //-------------------------------------------------------------------------------------------------
 
 
 #if OUTPUT
 
-10,30,0 -> 0,10,30
+{10,30,0} -> {0,10,30}
 
 #endif
