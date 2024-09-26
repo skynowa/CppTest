4b542a2fa8bc5c98c9dd06a843ccc73bec004a65
skynowa <skynowa@gmail.com>
2024-09-26 15:08:28 +0300

    conan-update: Backtrace_2.cpp [17+/16-] - review

--- OS/Unix/Backtrace_2.cpp
+++ OS/Unix/Backtrace_2.cpp
@@ -46,22 +46,23 @@ getFileLine(void* addr)
 void
 printStackTrace()
 {
-    const int max_frames = 10;
-    void* addrlist[max_frames];
+    const int  frames_max = 10;
+    void      *frames[frames_max] {};
 
     // Capture the stack trace
-    int addrlen = ::backtrace(addrlist, max_frames);
-
-    if (addrlen == 0) {
+    int addr_size = ::backtrace(frames, frames_max);
+    if (addr_size == 0) {
         std::cerr << "No stack trace available" << std::endl;
         return;
     }
 
-    for (int i = 0; i < addrlen; ++i) {
+    for (int i = 0; i < addr_size; ++i) {
+    	const auto frame = frames[i];
+
     	Dl_info info {};
-    	int iRv = dladdr(addrlist[i], &info);
+    	int iRv = dladdr(frame, &info);
         if (iRv == 0) {
-            std::printf("%-3d %p: [no symbol]\n", i, addrlist[i]);
+            std::printf("%-3d %p: [no symbol]\n", i, frame);
             continue;
         }
 
@@ -71,27 +72,27 @@ printStackTrace()
 		if (info.dli_sname) {
 			demangled_name = abi::__cxa_demangle(info.dli_sname, nullptr, 0, &status);
 			if (status == 0 && demangled_name) {
-				std::printf("%-3d %p: %s (+%ld) [%s]\n", i, addrlist[i], demangled_name,
-							(char*)addrlist[i] - (char*)info.dli_saddr, info.dli_fname);
+				std::printf("%-3d %p: %s (+%ld) [%s]\n", i, frame, demangled_name,
+							(char*)frame - (char*)info.dli_saddr, info.dli_fname);
 				std::free((void*)demangled_name);
 			}
 		}
 
 		if (status == 0 && demangled_name) {
-			std::printf("%-3d %p: %s (+%ld) [%s]\n", i, addrlist[i], demangled_name,
-						(char*)addrlist[i] - (char*)info.dli_saddr, info.dli_fname);
+			std::printf("%-3d %p: %s (+%ld) [%s]\n", i, frame, demangled_name,
+						(char*)frame - (char*)info.dli_saddr, info.dli_fname);
 			std::free((void*)demangled_name);
 		}
         else if (info.dli_sname) {
-			std::printf("%-3d %p: %s (+%ld) [%s]\n", i, addrlist[i], info.dli_sname,
-						(char*)addrlist[i] - (char*)info.dli_saddr, info.dli_fname);
+			std::printf("%-3d %p: %s (+%ld) [%s]\n", i, frame, info.dli_sname,
+						(char*)frame - (char*)info.dli_saddr, info.dli_fname);
 		}
         else {
-			std::printf("%-3d %p: ?? [%s]\n", i, addrlist[i], info.dli_fname);
+			std::printf("%-3d %p: ?? [%s]\n", i, frame, info.dli_fname);
 		}
 
 		// Get file and line information from addr2line
-		const std::string &file_line = getFileLine(addrlist[i]);
+		const std::string &file_line = getFileLine(frame);
 
 		std::cout << "    " << file_line;
     }
