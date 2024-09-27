651fe01acb358b421686cf14737a988aae936400
skynowa <skynowa@gmail.com>
2024-09-27 16:22:33 +0300

    boost-stacktrace: Stacktrace.cpp [2+/2-] - impl

--- Libs/Boost/Stacktrace.cpp
+++ Libs/Boost/Stacktrace.cpp
@@ -14,7 +14,7 @@
 	#include <boost/version.hpp>
 #endif
 
-#if 1 || (BOOST_VERSION > 0)
+#if (BOOST_VERSION > 0)
     // NOTE: BOOST_STACKTRACE_USE_ADDR2LINE - not working
 	#define BOOST_STACKTRACE_USE_BACKTRACE 1
 	#include <boost/stacktrace.hpp>
@@ -22,7 +22,7 @@
 //-------------------------------------------------------------------------------------------------
 int main(int , char **)
 {
-#if 1 || (BOOST_VERSION > 0)
+#if (BOOST_VERSION > 0)
     const auto &stackTrace = boost::stacktrace::stacktrace();
 
 	for (const auto &it_frame : stackTrace.as_vector()) {
