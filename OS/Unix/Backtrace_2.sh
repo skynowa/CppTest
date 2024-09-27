4009c78f9025969b4df1125d4a712ab85436f116
skynowa <skynowa@gmail.com>
2024-09-27 23:23:44 +0300

    boost-stacktrace: Backtrace_2.sh [26+/3-] - impl

--- OS/Unix/Backtrace_2.sh
+++ OS/Unix/Backtrace_2.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
+#
+# \file  Backtrace_2.sh
+# \brief Compile
+#
 
 
+#--------------------------------------------------------------------------------------------------
+# vars
 CPP_BASE_NAME="./Backtrace_2"
 
-echo "Compile ${CPP_BASE_NAME}.cpp ..."
-c++ -g -O0 -rdynamic ./${CPP_BASE_NAME}.cpp -ldl -fno-omit-frame-pointer -fno-inline -no-pie -fpermissive -o ./${CPP_BASE_NAME}.exe
+CPP_STANDART="c++20"
+INCLUDE_DIR="/home/skynowa/Projects/CppTest"
+#---------------------------------------------------------------------------------------------------
+# Clean
+if [ -f "${CPP_BASE_NAME}.exe" ]; then
+    echo "${CPP_BASE_NAME}.exe - Clean..."
 
-echo "Run..."
+    rm "${CPP_BASE_NAME}.exe"
+fi
+#---------------------------------------------------------------------------------------------------
+# Build
+echo "${CPP_BASE_NAME}.cpp - Compile..."
+
+c++ -std=${CPP_STANDART} -g -O0 -rdynamic ./${CPP_BASE_NAME}.cpp -ldl -fno-omit-frame-pointer -fno-inline -no-pie -fpermissive -I ${INCLUDE_DIR} -o ./${CPP_BASE_NAME}.exe
+#---------------------------------------------------------------------------------------------------
+# Run
+echo -e "${CPP_BASE_NAME}.exe - Run...\n"
+
 ./${CPP_BASE_NAME}.exe
+#---------------------------------------------------------------------------------------------------
+echo -e "\nOK.\n"
+#---------------------------------------------------------------------------------------------------
