7ae8e3974f54ab90a9a30fefe338a8d52f4cca86
skynowa <skynowa@gmail.com>
2024-09-27 23:24:08 +0300

    boost-stacktrace: Backtrace_2.cpp [182+/87-] - As classes

--- OS/Unix/Backtrace_2.cpp
+++ OS/Unix/Backtrace_2.cpp
@@ -13,39 +13,76 @@
 //-------------------------------------------------------------------------------------------------
 #include <StdStream/StdStream.h>
 #include <StdTest/StdTest.h>
-// #include <Stl.h>
-#include <fstream>
+#include <Stl.h>
 
 #include <dlfcn.h>
 #include <execinfo.h>
 #include <cxxabi.h>
 //-------------------------------------------------------------------------------------------------
-class mapping_entry_t
+
+/**************************************************************************************************
+*   MappingEntry
+*
+**************************************************************************************************/
+
+//-------------------------------------------------------------------------------------------------
+class MappingEntry
+	/// \see /proc/self/maps
 {
 public:
-	uintptr_t start {};
-	uintptr_t end {};
-	uintptr_t offset_from_base {};
+///\name ctors, dtor
+///\{
+	MappingEntry() = default;
+	MappingEntry(const std::string startStr, const std::string endStr,
+		const std::string offsetFromBaseStr);
+	~MappingEntry() = default;
+///\}
 
-public:
-	bool contains_addr(const void *a_addr) const
-	{
-		const uintptr_t addr_uint = reinterpret_cast<uintptr_t>(a_addr);
+	bool      containsAddr(const std::uintptr_t addr) const;
+	uintptr_t addr() const;
 
-		return (addr_uint >= start && addr_uint < end);
-	}
+private:
+	const uintptr_t _start {};
+	const uintptr_t _end {};
+	const uintptr_t _offsetFromBase {};
 
-	uintptr_t addr() const
-	{
-		return (start - offset_from_base);
-	}
+	uintptr_t _hexStrToInt(const std::string &str) const;
 };
 //-------------------------------------------------------------------------------------------------
-uintptr_t
-hex_str_to_int(
-	const std::string &a_str
-)
+MappingEntry::MappingEntry(
+	const std::string a_startStr,
+	const std::string a_endStr,
+	const std::string a_offsetFromBaseStr
+) :
+	_start          { _hexStrToInt(a_startStr) },
+	_end            { _hexStrToInt(a_endStr) },
+	_offsetFromBase { _hexStrToInt(a_offsetFromBaseStr) }
 {
+}
+//-------------------------------------------------------------------------------------------------
+bool
+MappingEntry::containsAddr(
+	const std::uintptr_t a_addr
+) const
+{
+	STD_TEST(a_addr != 0);
+
+	return (a_addr >= _start && a_addr < _end);
+}
+//-------------------------------------------------------------------------------------------------
+uintptr_t
+MappingEntry::addr() const
+{
+	return (_start - _offsetFromBase);
+}
+//-------------------------------------------------------------------------------------------------
+uintptr_t
+MappingEntry::_hexStrToInt(
+	const std::string &a_str
+) const
+{
+	STD_TEST(!a_str.empty());
+
 	uintptr_t out {};
 
 	std::stringstream ss;
@@ -62,16 +99,89 @@ hex_str_to_int(
 	throw std::invalid_argument(std::string("can't convert '") + a_str + "' to hex");
 }
 //-------------------------------------------------------------------------------------------------
-mapping_entry_t
-parse_proc_maps_line(
-	const std::string &a_line
-)
+
+
+/**************************************************************************************************
+*   Mappings
+*
+**************************************************************************************************/
+
+//-------------------------------------------------------------------------------------------------
+class Mappings
+	/// \see /proc/self/maps
+{
+public:
+///\name ctors, dtor
+///\{
+	Mappings() = delete;
+	Mappings(const std::uintptr_t addr, const bool position_independent);
+	~Mappings() = default;
+///\}
+
+	std::uintptr_t offset() const;
+
+private:
+	const std::uintptr_t _addr {};
+	const bool           _position_independent {};
+
+	std::uintptr_t _get_own_proc_addr_base(const std::uintptr_t addr) const;
+	MappingEntry   _parse_proc_maps_line(const std::string &map_entry) const;
+};
+//-------------------------------------------------------------------------------------------------
+Mappings::Mappings(
+	const std::uintptr_t a_addr,
+	const bool           a_position_independent
+) :
+	_addr                 {a_addr},
+	_position_independent {a_position_independent}
+{
+	STD_TEST(a_addr != 0);
+}
+//-------------------------------------------------------------------------------------------------
+std::uintptr_t
+Mappings::offset() const
+{
+	std::uintptr_t addr_base {};
+
+	if (_position_independent) {
+		addr_base = _get_own_proc_addr_base(_addr);
+	}
+
+	const uintptr_t offset = _addr - addr_base;
+
+	return offset;
+}
+//-------------------------------------------------------------------------------------------------
+std::uintptr_t
+Mappings::_get_own_proc_addr_base(
+	const std::uintptr_t a_addr
+) const
+{
+	std::ifstream maps_file("/proc/self/maps");
+	STD_TEST(maps_file.is_open());
+
+	for (std::string map_entry; std::getline(maps_file, map_entry); ) {
+		MappingEntry mapping = _parse_proc_maps_line(map_entry);
+		if ( mapping.containsAddr(a_addr) ) {
+			return mapping.addr();
+		}
+	}
+
+	STD_TEST(false);
+
+	return {};
+}
+//-------------------------------------------------------------------------------------------------
+MappingEntry
+Mappings::_parse_proc_maps_line(
+	const std::string &a_map_entry
+) const
 {
 	std::string mapping_range_str;
 	std::string permissions_str;
 	std::string offset_from_base_str;
 	{
-		std::istringstream line_stream(a_line);
+		std::istringstream line_stream(a_map_entry);
 		if (!std::getline(line_stream, mapping_range_str, ' ') ||
 			!std::getline(line_stream, permissions_str, ' ') ||
 			!std::getline(line_stream, offset_from_base_str, ' '))
@@ -92,64 +202,56 @@ parse_proc_maps_line(
 	}
 
 	try {
-		mapping_entry_t mapping {};
-		mapping.start            = ::hex_str_to_int(mapping_start_str);
-		mapping.end              = ::hex_str_to_int(mapping_end_str);
-		mapping.offset_from_base = ::hex_str_to_int(offset_from_base_str);
+		MappingEntry mapping(mapping_start_str, mapping_end_str, offset_from_base_str);
 
-		return mapping;
+		return std::move(mapping);
 	}
 	catch (const std::invalid_argument &e) {
 		return {};
 	}
 }
 //-------------------------------------------------------------------------------------------------
-std::uintptr_t
-get_own_proc_addr_base(
-	const void *a_addr
-)
-{
-	std::ifstream maps_file("/proc/self/maps");
-	STD_TEST(maps_file.is_open());
 
-	for (std::string line; std::getline(maps_file, line); ) {
-		const mapping_entry_t mapping = ::parse_proc_maps_line(line);
-		if ( mapping.contains_addr(a_addr) ) {
-			return mapping.addr();
-		}
-	}
 
-	STD_TEST(false);
+/**************************************************************************************************
+*   Impl
+*
+**************************************************************************************************/
 
-	return {};
-}
 //-------------------------------------------------------------------------------------------------
 // Function to execute addr2line and get file and line number
 std::string
-getFileLine(
+getAddr2line(
 	const void *a_frame
 )
 {
+	STD_TEST_PTR(a_frame);
+
 	std::string result;
 
-	char addrStr[20 + 1] {};
-	std::sprintf(addrStr, "%p", a_frame);
+	std::string cmd;
+	{
+		char addrStr[20 + 1] {};
+		std::sprintf(addrStr, "%p", a_frame);
 
-	const std::string cmd =
-		"addr2line "
-		"--exe=Backtrace_2.exe "
-		// "--functions --demangle --inlines --pretty-print "
-		+ std::string(addrStr);
-
-	FILE *pipe = ::popen(cmd.c_str(), "r");
-	STD_TEST_PTR(pipe);
-
-	char buffer[256] {};
-	while (std::fgets(buffer, sizeof(buffer), pipe) != nullptr) {
-		result += buffer;
+		cmd =
+			"addr2line "
+			"--exe=Backtrace_2.exe "
+			// "--functions --demangle --inlines --pretty-print "
+			+ std::string(addrStr);
 	}
 
-	::pclose(pipe);
+	{
+		FILE *pipe = ::popen(cmd.c_str(), "r");
+		STD_TEST_PTR(pipe);
+
+		char buffer[256] {};
+		while (std::fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+			result += buffer;
+		}
+
+		::pclose(pipe);
+	}
 
 	// Trimming
 	while (
@@ -164,37 +266,31 @@ getFileLine(
 }
 //-------------------------------------------------------------------------------------------------
 std::string
-source_location(
+getFileLine(
 	const void *a_addr,
 	const bool  a_position_independent
 )
 {
-	uintptr_t addr_base {};
+	STD_TEST_PTR(a_addr);
 
-	if (a_position_independent) {
-		addr_base = ::get_own_proc_addr_base(a_addr);
-	}
+	Mappings mappings(reinterpret_cast<std::uintptr_t>(a_addr), a_position_independent);
 
-	const auto offset = reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(a_addr) - addr_base);
-
-#if 0
-	std::string source_line = boost::stacktrace::detail::addr2line("-Cpe", reinterpret_cast<const void*>(offset));
-	if (source_line.empty() || source_line[0] == '?') {
-		return "";
-	}
-
-	return source_line;
-#else
-	const std::string source_line = ::getFileLine(offset);
-	if (source_line.empty() ||
-		source_line[0] == '?')
+	const std::string sRv = ::getAddr2line( reinterpret_cast<void *>(mappings.offset()) );
+	if (sRv.empty() ||
+		sRv[0] == '?')
 	{
 		return {};
 	}
 
-	return source_line;
-#endif
+	return sRv;
 }
+//-------------------------------------------------------------------------------------------------
+
+/**************************************************************************************************
+*   Test
+*
+**************************************************************************************************/
+
 //-------------------------------------------------------------------------------------------------
 void
 printStackTrace()
@@ -246,17 +342,16 @@ printStackTrace()
 
 		// Get file and line information from addr2line
 		{
-			// const std::string &fileLine = ::getFileLine(frame);
-			std::string fileLine = ::source_location(frame, false);
+			std::string fileLine = ::getFileLine(frame, false);
 			if ( fileLine.empty() ) {
-				fileLine = ::source_location(frame, true);
+				fileLine = ::getFileLine(frame, true);
 			}
 
 			if (fileLine.empty()) {
-				fileLine = "[0]";
+				fileLine = "[n/a]";
 			}
 
-			std::cout << "\t" << STD_TRACE_VAR(fileLine) << std::endl;
+			std::cout << "\t" << "at " << fileLine << std::endl;
 		}
 	}
 }
