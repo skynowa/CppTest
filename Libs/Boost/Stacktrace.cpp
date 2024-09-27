4377c128a78a59e570afe91440846ef53182fd92
skynowa <skynowa@gmail.com>
2024-09-27 16:48:30 +0300

    boost-stacktrace: Stacktrace.cpp [43+/11-] - impl

--- Libs/Boost/Stacktrace.cpp
+++ Libs/Boost/Stacktrace.cpp
@@ -14,31 +14,63 @@
 	#include <boost/version.hpp>
 #endif
 
-#if (BOOST_VERSION > 0)
-    // NOTE: BOOST_STACKTRACE_USE_ADDR2LINE - not working
+#if 1 || (BOOST_VERSION > 0)
+	// NOTE: BOOST_STACKTRACE_USE_ADDR2LINE - not working
 	#define BOOST_STACKTRACE_USE_BACKTRACE 1
 	#include <boost/stacktrace.hpp>
 #endif
 //-------------------------------------------------------------------------------------------------
+std::string
+executableName(
+	const boost::stacktrace::frame &a_frame
+)
+{
+	// The first frame usually represents the main function or entry point
+	if ( a_frame.empty() ) {
+		return "Unknown executable";  // Fallback if unable to determine
+	}
+
+	const auto addr = a_frame.address();
+	if (addr == nullptr) {
+		return {};
+	}
+
+	Dl_info info {};
+	if (::dladdr(addr, &info) == 0) {
+		return {};
+	}
+
+	if (info.dli_fname != nullptr) {
+		return {};
+	}
+
+	return info.dli_fname;
+}
+//-------------------------------------------------------------------------------------------------
 int main(int , char **)
 {
-#if (BOOST_VERSION > 0)
-    const auto &stackTrace = boost::stacktrace::stacktrace();
+#if 1 || (BOOST_VERSION > 0)
+	const auto &stackTrace = boost::stacktrace::stacktrace();
 
 	for (const auto &it_frame : stackTrace.as_vector()) {
-		std::cout << STD_TRACE_VAR(it_frame) << '\n' << std::endl;
+		std::cout << "------------------------------" << std::endl;
 
-		std::cout <<
-			STD_TRACE_VAR(it_frame.name()) << "\n"
-			STD_TRACE_VAR(it_frame.address()) << "\n"
-			STD_TRACE_VAR(it_frame.source_file()) << "\n"
-			STD_TRACE_VAR(it_frame.source_line()) << std::endl;
+		if (0) {
+			std::cout << STD_TRACE_VAR(it_frame) << '\n' << std::endl;
+		} else {
+			std::cout <<
+				"module: " << ::executableName(it_frame) << "\n"
+				STD_TRACE_VAR(it_frame.name()) << "\n"
+				STD_TRACE_VAR(it_frame.address()) << "\n"
+				STD_TRACE_VAR(it_frame.source_file()) << "\n"
+				STD_TRACE_VAR(it_frame.source_line()) << std::endl;
+		}
 	}
 #else
 	std::cout << "Boost - not instaled, skip" << std::endl;
 #endif
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 //-------------------------------------------------------------------------------------------------
 
