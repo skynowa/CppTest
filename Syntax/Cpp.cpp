ad38219be8b04d3691c2d58a5bc63ee2b9885694
skynowa <skynowa@gmail.com>
2024-10-14 21:28:05 +0300

    boost-stacktrace: Cpp.cpp [33+/3-] - impl

--- Syntax/Cpp.cpp
+++ Syntax/Cpp.cpp
@@ -8,14 +8,24 @@
  */
 
 
+//--------------------------------------------------------------------------------------------------
+// PP
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpessimizing-move"
+
+// Impl - std::move
+
+#pragma GCC diagnostic pop
+
 #include <StdStream/StdStream.h>
 #include <StdTest/StdTest.h>
 #include <Stl.h>
-//--------------------------------------------------------------------------------------------------
-// PP
+
 #define PP_VAR      "pp_test"
 #define PP_VAR_2(x) PP_VAR "_" x
 //--------------------------------------------------------------------------------------------------
+using namespace std::literals::string_view_literals; // Enables the sv suffix
+
 namespace my
 {
 int varNamespace {5};
@@ -37,7 +47,8 @@ public:
     SyntaxCpp() = default;
     ~SyntaxCpp() = default;
 
-    bool fooMethod(const bool isDebug) const;
+    std::string_view get() const;
+    bool             fooMethod(const bool isDebug) const;
 
     static int fooMethodStatic(bool &isDebug);
 
@@ -45,10 +56,17 @@ private:
     Type _type   {Type::No};
     int  _member {0};
 
+
     static int _memberStatic;
 };
 //--------------------------------------------------------------------------------------------------
 int SyntaxCpp::_memberStatic {-1};
+//--------------------------------------------------------------------------------------------------
+std::string_view
+SyntaxCpp::get() const
+{
+    return "[SyntaxCpp]"sv;
+}
 //--------------------------------------------------------------------------------------------------
 bool
 SyntaxCpp::fooMethod(
@@ -68,6 +86,16 @@ SyntaxCpp::fooMethodStatic(
     out_isDebug = false;
 
     return out_isDebug ? 0 : _memberStatic;
+}
+//--------------------------------------------------------------------------------------------------
+inline std::ostream &
+operator << (
+    std::ostream    &out_os,
+    const SyntaxCpp &a_value
+)
+{
+	out_os << a_value.get();
+	return out_os;
 }
 //--------------------------------------------------------------------------------------------------
 
@@ -91,6 +119,8 @@ int main(int, char **)
     SyntaxCpp syntax;
     syntax.fooMethod(::globalVar);
     syntax.fooMethodStatic(::globalVar);
+
+    std::cout << syntax << std::endl;
 
     return EXIT_SUCCESS;
 }
