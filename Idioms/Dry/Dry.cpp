772c292b4feeed8577f4792df45e5845a08ecb38
skynowa <skynowa@gmail.com>
2026-05-04 23:46:33 +0300

    Dry.cpp (+2) [27+/11-] - impl

--- Idioms/Dry/Dry.cpp
+++ Idioms/Dry/Dry.cpp
@@ -1,8 +1,6 @@
 /**
  * \file  Dry.cpp
  * \brief DRY - Don't repeat yourself
- *
- * \review
  */
 
 
@@ -10,11 +8,22 @@
 #include <StdTest/StdTest.h>
 #include <Stl.h>
 //--------------------------------------------------------------------------------------------------
+double priceWithTax(double a_price, double a_taxRate)
+{
+	return a_price * (1.0 + a_taxRate);
+}
+//--------------------------------------------------------------------------------------------------
+void printPrice(const std::string &a_name, double a_price, double a_taxRate)
+{
+	std::cout << a_name << ": " << priceWithTax(a_price, a_taxRate) << std::endl;
+}
+//--------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
+	const double taxRate {0.2};
 
-
-    // std::cout << STD_TRACE_VAR("") << std::endl;
+	printPrice("book", 10.0, taxRate);
+	printPrice("pen", 2.5, taxRate);
 
     return EXIT_SUCCESS;
 }
@@ -23,6 +32,7 @@ int main(int, char **)
 
 #if OUTPUT
 
-
+book: 12
+pen: 3
 
 #endif
--- Idioms/Kiss/Kiss.cpp
+++ Idioms/Kiss/Kiss.cpp
@@ -1,8 +1,6 @@
 /**
  * \file  Kiss.cpp
  * \brief keep it simple stupid
- *
- * \review
  */
 
 
@@ -10,11 +8,16 @@
 #include <StdTest/StdTest.h>
 #include <Stl.h>
 //--------------------------------------------------------------------------------------------------
+bool isEven(int a_value)
+{
+	return a_value % 2 == 0;
+}
+//--------------------------------------------------------------------------------------------------
 int main(int, char **)
 {
-
-
-    // std::cout << STD_TRACE_VAR("") << std::endl;
+	for (const int value : {1, 2, 3, 4}) {
+		std::cout << STD_TRACE_VAR(value) << ", " << STD_TRACE_VAR(isEven(value)) << std::endl;
+	}
 
     return EXIT_SUCCESS;
 }
@@ -23,6 +26,9 @@ int main(int, char **)
 
 #if OUTPUT
 
-
+value: 1, isEven(value): 0
+value: 2, isEven(value): 1
+value: 3, isEven(value): 0
+value: 4, isEven(value): 1
 
 #endif
